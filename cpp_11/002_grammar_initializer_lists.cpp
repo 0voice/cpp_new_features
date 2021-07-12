694b7763ed0d388fca2a3afbb9c13ca50cee0d3e
hanaper <hanaper@qq.com>
2021-07-12 20:34:12 +0800

    Update 002_grammar_initializer_lists.cpp

--- cpp_11/002_grammar_initializer_lists.cpp
+++ cpp_11/002_grammar_initializer_lists.cpp
@@ -1,63 +1,26 @@
 #include <iostream>
 #include <vector>
-
 #include <list>
 #include <map>
+
 using namespace std;
 
 int main(int argc, char *argv[])
-{ 
-#if 0
-　　vector<int> vi(5);
-　　cout<<vi.size()<<vi.capacity()<<endl;
-　　vector<int> vi2(5,10);
-　　for(auto i: vi2){
-　　　　cout<<i<<endl;
-　　} 
-　　vector<int> vi3;
-　　for(int i=0; i<10; i++){
-　　　　vi3.push_back(i);
-　　} 
-　　for(auto i: vi3){
-　　　　cout<<i<<endl;
-　　} 
-　　list<int> li(5);
-　　cout<<li.size()<<endl;
-　　for(auto &i:li){
-　　　　cout<<i<<endl;
-　　} 
-　　list<int> li2(5,10);
-　　cout<<li2.size()<<endl;
-　　for(auto &i:li2){
-　　cout<<i<<endl;
-　　} 
-　　list<int> li3;
-　　for(int i=0; i<10; i++)
-　　{
-　　　　li3.push_back(i);
-　　} 
-　　cout<<li3.size()<<endl;
-　　for(auto &i:li3){
-　　　　cout<<i<<endl;
-　　}
-#endif
-　　map<int,string> mis;
-
-　　mis.insert(pair<int,string>(1,"c++"));
-　　mis.insert(pair<int,string>(2,"java"));
-　　mis.insert(pair<int,string>(3,"python"));
-　　mis.insert(map<int,string>::value_type(4,"c"));
-　　mis.insert(map<int,string>::value_type(5,"php"));
-　　for(auto is: mis)
-　　{
-　　cout<<is.first<<"\t"<<is.second<<endl;
-　　} 
-　　mis[6] = "scala";
-　　mis[7] = "basic";
-　　mis[8] = "ruby";
-　　for(auto &is: mis)
-　　{
-　　cout<<is.first<<"\t"<<is.second<<endl;
-　　} 
-　　return 0;
+{
+    vector<int> iv = {1,2,3,4,5};
+    list<int> li = {1,2,3,4,5};
+    map<int,string> mis = {{1,"c"},{2,"c++"},
+                                       {3,"java"},{4,"scala"},
+                                       {5,"python"}};
+    mis.insert({6,"ruby"});
+    // map<int,string>::iterator itr = mis.begin();
+    // for(; itr != mis.end(); ++itr)
+    // {
+    // cout<<itr->first<< itr->second<<endl;
+    // }
+    for(auto &is: mis)
+    {
+        cout<<is.first<<is.second<<endl;
+    }
+     return 0;
 }
