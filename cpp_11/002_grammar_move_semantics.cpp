38c0c06b1e46445e6a3c0e1e11acf32652dad3f9
hanaper <hanaper@qq.com>
2021-07-12 22:14:27 +0800

    Update 002_grammar_move_semantics.cpp

--- cpp_11/002_grammar_move_semantics.cpp
+++ cpp_11/002_grammar_move_semantics.cpp
@@ -1,1 +1,87 @@
-1
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class MyString
+{
+public:
+    static size_t CCtor; //统计调用拷贝构造函数的次数
+    static size_t MCtor; //统计调用移动构造函数的次数
+    static size_t CAsgn; //统计调用拷贝赋值函数的次数
+    static size_t MAsgn; //统计调用移动赋值函数的次数
+
+public:
+    // 构造函数
+   MyString(const char* cstr=0){
+       if (cstr) {
+          m_data = new char[strlen(cstr)+1];
+          strcpy(m_data, cstr);
+       }
+       else {
+          m_data = new char[1];
+          *m_data = '\0';
+       }
+   }
+
+   // 拷贝构造函数
+   MyString(const MyString& str) {
+       CCtor ++;
+       m_data = new char[ strlen(str.m_data) + 1 ];
+       strcpy(m_data, str.m_data);
+   }
+   // 移动构造函数
+   MyString(MyString&& str) noexcept
+       :m_data(str.m_data) {
+       MCtor ++;
+       str.m_data = nullptr; //不再指向之前的资源了
+   }
+
+   // 拷贝赋值函数 =号重载
+   MyString& operator=(const MyString& str){
+       CAsgn ++;
+       if (this == &str) // 避免自我赋值!!
+          return *this;
+
+       delete[] m_data;
+       m_data = new char[ strlen(str.m_data) + 1 ];
+       strcpy(m_data, str.m_data);
+       return *this;
+   }
+
+   // 移动赋值函数 =号重载
+   MyString& operator=(MyString&& str) noexcept{
+       MAsgn ++;
+       if (this == &str) // 避免自我赋值!!
+          return *this;
+
+       delete[] m_data;
+       m_data = str.m_data;
+       str.m_data = nullptr; //不再指向之前的资源了
+       return *this;
+   }
+
+   ~MyString() {
+       delete[] m_data;
+   }
+
+   char* get_c_str() const { return m_data; }
+private:
+   char* m_data;
+};
+size_t MyString::CCtor = 0;
+size_t MyString::MCtor = 0;
+size_t MyString::CAsgn = 0;
+size_t MyString::MAsgn = 0;
+int main()
+{
+    vector<MyString> vecStr;
+    vecStr.reserve(1000); //先分配好1000个空间
+    for(int i=0;i<1000;i++){
+        vecStr.push_back(MyString("hello"));
+    }
+    cout << "CCtor = " << MyString::CCtor << endl;
+    cout << "MCtor = " << MyString::MCtor << endl;
+    cout << "CAsgn = " << MyString::CAsgn << endl;
+    cout << "MAsgn = " << MyString::MAsgn << endl;
+}
