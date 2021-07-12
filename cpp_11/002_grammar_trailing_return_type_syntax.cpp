5c15075c40dd49a7905f9209817ff76c1e07571b
hanaper <hanaper@qq.com>
2021-07-12 20:01:11 +0800

    Update 002_grammar_trailing_return_type_syntax.cpp

--- cpp_11/002_grammar_trailing_return_type_syntax.cpp
+++ cpp_11/002_grammar_trailing_return_type_syntax.cpp
@@ -1,3 +1,41 @@
-int main() { 
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+int func(int, int);
+auto func2(int, int)->int; // 指定返回类型
+
+template<typename T1, typename T2>
+auto sum(const T1 &t1, const T2 &t2)->decltype(t1+t2) // 指定返回类型
+{
+    return t1 + t2;
+}
+
+template<typename T1, typename T2>
+auto mul(const T1 &t1, const T2 &t2)->decltype(t1*t2) // 指定返回类型
+{
+    return t1 * t2;
+}
+
+void mytest()
+{
+    auto a = 3;
+    auto b = 4L;
+    auto pi = 3.14f;
+
+    auto c = mul(sum(a, b), pi);
+    std::cout << c << std::endl; // 21.98
+
+    return;
+}
+
+int main()
+{
+    mytest();
+
+    system("pause");
+    return 0;
 }
