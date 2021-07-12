3f2721a2cb9f46172c71aae46011133ea4f1b90d
hanaper <hanaper@qq.com>
2021-07-12 20:56:51 +0800

    Update 002_grammar_lnline.cpp

--- cpp_11/002_grammar_lnline.cpp
+++ cpp_11/002_grammar_lnline.cpp
@@ -1,3 +1,17 @@
-int main() { 
-
+namespace Parent
+{
+ 
+    inline namespace Child1
+    {
+        struct child1_data{int a;} ;
+    }
+    inline namespace Child2
+    {
+        struct child2_data{int b;} ;
+    }
+    namespace child3
+    {
+        child1_data data1;   
+        child2_data data2;   
+    }
 }
