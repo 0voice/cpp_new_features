b43d07d258517ff1714a802157761b0eff888166
hanaper <hanaper@qq.com>
2021-07-12 20:26:42 +0800

    Update 002_grammar_extended_friend_syntax.cpp

--- cpp_11/002_grammar_extended_friend_syntax.cpp
+++ cpp_11/002_grammar_extended_friend_syntax.cpp
@@ -1,3 +1,17 @@
-int main() { 
+class Poly;
+typedef Poly P; 
 
-}
+class LiLei 
+{
+	friend class Poly; // C++98通过, C++11通过 
+}; 
+
+class Jim 
+{ 
+	friend Poly;       // C++98失败, C++11通过 
+};
+
+class HanMei
+{ 
+	friend P;          // C++98失败, C++11通过 
+};
