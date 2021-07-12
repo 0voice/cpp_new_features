317c6956ba7c82918aaf410a78da1d9e93c3d871
hanaper <hanaper@qq.com>
2021-07-12 20:19:43 +0800

    Update 002_grammar_initializer_lists.cpp

--- cpp_11/002_grammar_initializer_lists.cpp
+++ cpp_11/002_grammar_initializer_lists.cpp
@@ -1,4 +1,9 @@
 int main() { 
+	int arr[] = { 1, 2, 3, 4, 5 };
+	std::map < int, int > map_t { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+	std::list<std::string> list_str{ "hello", "world", "china" };
+	std::vector<double> vec_d { 0.0,0.1,0.2,0.3,0.4,0.5};
+
 	int b1{1};
 	int b2 = {2};
 	int *b3 = new int{3};
