ee91425edb58e186641f6337db0bf578c11cad85
hanaper <hanaper@qq.com>
2021-07-12 21:19:33 +0800

    Update 002_grammar_stdref_stdcref.cpp

--- cpp_11/002_grammar_stdref_stdcref.cpp
+++ cpp_11/002_grammar_stdref_stdcref.cpp
@@ -1,3 +1,23 @@
-int main() { 
+//std::ref 用于包装按引用传递的值。 
+//std::cref 用于包装按const引用传递的值。
 
+#include<iostream>
+#include<thread>
+#include<string>
+
+using namespace std;
+
+void foo( int &a)
+{
+    cout<<"thread :"<< a++ <<endl;
+}
+
+int main()
+{
+    int num = 0;
+    thread t1(foo, std::ref(num));
+    thread t2(foo, std::ref(num));
+    t1.join();
+    t2.join();
+    return 0;
 }
