80d1f69923db7e874ee2faff6c9624c2780304c2
hanaper <hanaper@qq.com>
2021-07-12 19:52:30 +0800

    Update 002_grammar_alignas.cpp

--- cpp_11/002_grammar_alignas.cpp
+++ cpp_11/002_grammar_alignas.cpp
@@ -1,3 +1,10 @@
-int main() { 
-
+#include <iostream>
+ 
+int main()
+{
+  
+  alignas(double) unsigned char c[1024];	//字符数组，但是却以double数据的形式对齐数据
+  alginas(16) char d[100];				        //以16字节对齐
+  
+  return 0;
 }
