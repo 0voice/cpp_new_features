33f4e7d99e38f82443620a501a30a2a2856831e9
hanaper <hanaper@qq.com>
2021-07-12 19:42:51 +0800

    Create 002_grammar_alignof.cpp

--- cpp_11/002_grammar_alignof.cpp
+++ cpp_11/002_grammar_alignof.cpp
@@ -1,3 +1,28 @@
-int main() { 
-
+#include <iostream>
+ 
+struct Foo {
+    int   i;
+    float f;
+    char  c;
+};
+ 
+// Note: `alignas(alignof(long double))` below can be simplified to simply 
+// `alignas(long double)` if desired.
+struct alignas(alignof(long double)) Foo2 {
+    // put your definition here
+}; 
+ 
+struct Empty {};
+ 
+struct alignas(64) Empty64 {};
+ 
+int main()
+{
+    std::cout << "Alignment of"  "\n"
+        "- char             : " << alignof(char)    << "\n"
+        "- pointer          : " << alignof(int*)    << "\n"
+        "- class Foo        : " << alignof(Foo)     << "\n"
+        "- class Foo2       : " << alignof(Foo2)    << "\n"
+        "- empty class      : " << alignof(Empty)   << "\n"
+        "- alignas(64) Empty: " << alignof(Empty64) << "\n";
 }
