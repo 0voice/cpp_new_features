702e00fa0445495b446e7545fc9ab480a893042a
hanaper <hanaper@qq.com>
2021-07-12 20:32:56 +0800

    Update 002_grammar_initializer_lists.cpp

--- cpp_11/002_grammar_initializer_lists.cpp
+++ cpp_11/002_grammar_initializer_lists.cpp
@@ -1,11 +1,63 @@
-int main() { 
-	int arr[] = { 1, 2, 3, 4, 5 };
-	std::map < int, int > map_t { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
-	std::list<std::string> list_str{ "hello", "world", "china" };
-	std::vector<double> vec_d { 0.0,0.1,0.2,0.3,0.4,0.5};
+#include <iostream>
+#include <vector>
 
-	int b1{1};
-	int b2 = {2};
-	int *b3 = new int{3};
-	int b4 = int{4};
+#include <list>
+#include <map>
+using namespace std;
+
+int main(int argc, char *argv[])
+{ 
+#if 0
+　　vector<int> vi(5);
+　　cout<<vi.size()<<vi.capacity()<<endl;
+　　vector<int> vi2(5,10);
+　　for(auto i: vi2){
+　　　　cout<<i<<endl;
+　　} 
+　　vector<int> vi3;
+　　for(int i=0; i<10; i++){
+　　　　vi3.push_back(i);
+　　} 
+　　for(auto i: vi3){
+　　　　cout<<i<<endl;
+　　} 
+　　list<int> li(5);
+　　cout<<li.size()<<endl;
+　　for(auto &i:li){
+　　　　cout<<i<<endl;
+　　} 
+　　list<int> li2(5,10);
+　　cout<<li2.size()<<endl;
+　　for(auto &i:li2){
+　　cout<<i<<endl;
+　　} 
+　　list<int> li3;
+　　for(int i=0; i<10; i++)
+　　{
+　　　　li3.push_back(i);
+　　} 
+　　cout<<li3.size()<<endl;
+　　for(auto &i:li3){
+　　　　cout<<i<<endl;
+　　}
+#endif
+　　map<int,string> mis;
+
+　　mis.insert(pair<int,string>(1,"c++"));
+　　mis.insert(pair<int,string>(2,"java"));
+　　mis.insert(pair<int,string>(3,"python"));
+　　mis.insert(map<int,string>::value_type(4,"c"));
+　　mis.insert(map<int,string>::value_type(5,"php"));
+　　for(auto is: mis)
+　　{
+　　cout<<is.first<<"\t"<<is.second<<endl;
+　　} 
+　　mis[6] = "scala";
+　　mis[7] = "basic";
+　　mis[8] = "ruby";
+　　for(auto &is: mis)
+　　{
+　　cout<<is.first<<"\t"<<is.second<<endl;
+　　} 
+　　return 0;
 }
