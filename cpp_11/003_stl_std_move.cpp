612d12379cee0b19a0e5c7b07f63812383d48acc
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-17 18:02:59 +0800

    Update 003_stl_std_move.cpp

--- cpp_11/003_stl_std_move.cpp
+++ cpp_11/003_stl_std_move.cpp
@@ -1,1 +1,17 @@
-1
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+int main()
+{
+    std::string str = "Hello";
+    std::vector<std::string> v;
+    //调用常规的拷贝构造函数，新建字符数组，拷贝数据
+    v.push_back(str);
+    std::cout << "After copy, str is \"" << str << "\"\n";
+    //调用移动构造函数，掏空str，掏空后，最好不要使用str
+    v.push_back(std::move(str));
+    std::cout << "After move, str is \"" << str << "\"\n";
+    std::cout << "The contents of the vector are \"" << v[0]
+                                         << "\", \"" << v[1] << "\"\n";
+}
