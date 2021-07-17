9106d6e5aacc7e0b430d923d6d25d74318d27b4b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-17 18:00:22 +0800

    Update 003_stl_std_forward_list.cpp

--- cpp_11/003_stl_std_forward_list.cpp
+++ cpp_11/003_stl_std_forward_list.cpp
@@ -1,1 +1,12 @@
-1
+#include <forward_list>
+#include <iostream>
+ 
+int main()
+{
+    std::forward_list<int> numbers;
+    std::cout << "Initially, numbers.empty(): " << numbers.empty() << '\n';
+ 
+    numbers.push_front(42);
+    numbers.push_front(13317); 
+    std::cout << "After adding elements, numbers.empty(): " << numbers.empty() << '\n';
+}
