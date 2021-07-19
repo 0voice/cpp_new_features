3b6da243ec0cbc64e830ebcbc7925c3f02c43be6
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:23:06 +0800

    Update 003_template_angle_bracket.cpp

--- cpp_11/003_template_angle_bracket.cpp
+++ cpp_11/003_template_angle_bracket.cpp
@@ -1,1 +1,18 @@
-1
+template<typename T>
+struct Foo
+{
+      typedef T type;
+};
+
+template <typename T>
+class A
+{
+    // ...
+};
+
+int main()
+{
+    Foo<A<int>>::type xx;  //C++11之前编译出错,要写成  Foo<A<int> >
+    return 0;
+}
+
