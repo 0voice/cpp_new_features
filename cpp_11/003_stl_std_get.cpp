61d1678724a860f615918c9a8cc565f2f5b916ac
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:23:52 +0800

    Update 003_stl_std_get.cpp

--- cpp_11/003_stl_std_get.cpp
+++ cpp_11/003_stl_std_get.cpp
@@ -1,1 +1,15 @@
-1
+#include <iostream>
+#include <string>
+#include <tuple>
+ 
+int main()
+{
+    auto t = std::make_tuple(1, "Foo", 3.14);
+    // index-based access
+    std::cout << "(" << std::get<0>(t) << ", " << std::get<1>(t)
+              << ", " << std::get<2>(t) << ")\n";
+    // type-based access (C++14 or later)
+    std::cout << "(" << std::get<int>(t) << ", " << std::get<const char*>(t)
+              << ", " << std::get<double>(t) << ")\n";
+    // Note: std::tie and structured binding may also be used to decompose a tuple
+}
