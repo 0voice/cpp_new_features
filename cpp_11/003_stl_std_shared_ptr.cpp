d7a8574c34fd20b1e09f24f0fa488fb768a3854b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:37:10 +0800

    Update 003_stl_std_shared_ptr.cpp

--- cpp_11/003_stl_std_shared_ptr.cpp
+++ cpp_11/003_stl_std_shared_ptr.cpp
@@ -1,1 +1,15 @@
-1
+#include <memory> 
+#include <iostream> 
+ 
+void fun(std::shared_ptr<int> sp)
+{
+    std::cout << "fun: sp.use_count() == " << sp.use_count() << '\n'; 
+}
+ 
+int main() 
+{ 
+    auto sp1 = std::make_shared<int>(5);
+    std::cout << "sp1.use_count() == " << sp1.use_count() << '\n'; 
+ 
+    fun(sp1);
+}
