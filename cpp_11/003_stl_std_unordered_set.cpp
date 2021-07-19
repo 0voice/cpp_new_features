4d1da8a833f285943e4222daf88dc6dba96a470b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:17:55 +0800

    Update 003_stl_std_unordered_set.cpp

--- cpp_11/003_stl_std_unordered_set.cpp
+++ cpp_11/003_stl_std_unordered_set.cpp
@@ -1,1 +1,14 @@
-1
+#include <set>
+#include <iostream>
+int main()
+{
+    std::multiset<int> c = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    // 从 c 擦除所有奇数
+    for(auto it = c.begin(); it != c.end(); )
+        if(*it % 2 == 1)
+            it = c.erase(it);
+        else
+            ++it;
+    for(int n : c)
+        std::cout << n << ' ';
+}
