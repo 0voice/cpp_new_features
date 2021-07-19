7ebc6749e929b30ee95a2f87611b38ed350420ac
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:25:50 +0800

    Update 003_template_variable_parameter_template.cpp

--- cpp_11/003_template_variable_parameter_template.cpp
+++ cpp_11/003_template_variable_parameter_template.cpp
@@ -1,1 +1,20 @@
-1
+#include <iostream>
+
+int end_fun(int& a){
+    return a;
+}
+
+template<typename T,typename... Args>
+int end_fun(T& arg,Args... args){
+    return arg + end_fun(args...);
+}
+
+
+int main(int argc,char *argv[]){
+
+    char a = 'a';
+    int b = 2;
+    int c = 3;
+    int res = end_fun(a,b,c);
+    std::cout<<"Res: "<<res<<std::endl;
+}
