6b5cd5c82d894e12e19b56cccc47ccb7de486ddc
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:28:47 +0800

    Update 003_stl_std_hash_std_bitset.cpp

--- cpp_11/003_stl_std_hash_std_bitset.cpp
+++ cpp_11/003_stl_std_hash_std_bitset.cpp
@@ -1,1 +1,19 @@
-1
+#include <bitset>
+#include <iostream>
+#include <functional>
+ 
+int main()
+{
+    std::bitset<4> b1{0}, b2{42};
+    std::bitset<8> b3{0}, b4{42};
+ 
+    std::hash<std::bitset<4>> hash_fn4;
+    std::hash<std::bitset<8>> hash_fn8;
+    using bin64 = std::bitset<64>;
+ 
+    std::cout
+        << bin64{hash_fn4(b1)} << " = " << hash_fn4(b1) << '\n'
+        << bin64{hash_fn4(b2)} << " = " << hash_fn4(b2) << '\n'
+        << bin64{hash_fn8(b3)} << " = " << hash_fn8(b3) << '\n'
+        << bin64{hash_fn8(b4)} << " = " << hash_fn8(b4) << '\n';
+}
