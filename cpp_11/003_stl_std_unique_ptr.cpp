00efec542e0c36504cd983294875b71d6e7123f5
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:39:36 +0800

    Update 003_stl_std_unique_ptr.cpp

--- cpp_11/003_stl_std_unique_ptr.cpp
+++ cpp_11/003_stl_std_unique_ptr.cpp
@@ -1,1 +1,16 @@
-1
+struct Foo {
+    Foo(int _val) : val(_val) { std::cout << "Foo...\n"; }
+    ~Foo() { std::cout << "~Foo...\n"; }
+    int val;
+};
+ 
+int main()
+{
+    std::unique_ptr<Foo> up1(new Foo(1));
+    std::unique_ptr<Foo> up2(new Foo(2));
+ 
+    up1.swap(up2);
+ 
+    std::cout << "up1->val:" << up1->val << std::endl;
+    std::cout << "up2->val:" << up2->val << std::endl;
+}
