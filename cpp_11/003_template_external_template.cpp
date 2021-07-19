d92508ccadf29471091c0ad04f9b063c0347307e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:24:32 +0800

    Update 003_template_external_template.cpp

--- cpp_11/003_template_external_template.cpp
+++ cpp_11/003_template_external_template.cpp
@@ -1,1 +1,17 @@
-1
+// test.h
+template <typename T> 
+void fun(T) { }
+
+// test1.cpp
+#include "test.h" 
+void test1() 
+{ 
+    fun(1); 
+}
+
+// test2.cpp
+#include "test.h" 
+void test2()
+{
+    fun(2);
+}
