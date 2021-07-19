a05aaf37b186a02a6f28b999144baa2641f408c0
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:12:15 +0800

    Update 003_class_member_init.cpp

--- cpp_11/003_class_member_init.cpp
+++ cpp_11/003_class_member_init.cpp
@@ -1,1 +1,27 @@
-1
+#include <string>
+using namespace std;
+
+class Mem
+{
+public:
+    Mem(int i) : m(i)
+    {}
+
+ private:
+    int m;
+};
+
+class Group
+{
+public:
+    Group() {}                
+    Group(int a) : data(a) {} 
+    Group(Mem m) : mem(m) {}  
+    Group(int a, Mem m, string n) : data(a), mem(m), name(n)
+    {}
+
+private:
+    int data = 1;
+    Mem mem{0};
+    string name{" Group"};
+};
