07f4c2e8fe1a4a5e8f8ff84d521aa513a4b4ce58
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:16:34 +0800

    Update 003_stl_std_unordered_map.cpp

--- cpp_11/003_stl_std_unordered_map.cpp
+++ cpp_11/003_stl_std_unordered_map.cpp
@@ -1,1 +1,28 @@
-1
+#include <iostream>
+#include <string>
+#include <unordered_map>
+ 
+int main()
+{
+    // 创建三个 string 的 unordered_map （映射到 string ）
+    std::unordered_map<std::string, std::string> u = {
+        {"RED","#FF0000"},
+        {"GREEN","#00FF00"},
+        {"BLUE","#0000FF"}
+    };
+ 
+    // 迭代并打印 unordered_map 的关键和值
+    for( const auto& n : u ) {
+        std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
+    }
+ 
+    // 添加新入口到 unordered_map
+    u["BLACK"] = "#000000";
+    u["WHITE"] = "#FFFFFF";
+ 
+    // 用关键输出值
+    std::cout << "The HEX of color RED is:[" << u["RED"] << "]\n";
+    std::cout << "The HEX of color BLACK is:[" << u["BLACK"] << "]\n";
+ 
+    return 0;
+}
