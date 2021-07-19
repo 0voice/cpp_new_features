c8794ac6a1287a17368787f17d014ea479d90364
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:13:25 +0800

    Update 003_class_functor.cpp

--- cpp_11/003_class_functor.cpp
+++ cpp_11/003_class_functor.cpp
@@ -1,1 +1,22 @@
-1
+class Tax
+{
+private:
+    float rate;
+    int base;
+public:
+    Tax(float r, int b)
+        :rate(r),
+        base(b)
+    {}
+    float operator()(float money){ return (money - base)*rate; }
+};
+
+int main(int argc, char *argv[])
+{
+    //===functor
+    Tax high(0.40, 30000);
+    Tax middle(0.25, 20000);
+
+    cout << "tax over 3w: " << high(37500) << endl;
+    cout << "tax over 2w: " << middle(27500) << endl;
+}
