a5004f2b31e0ccf813cf0112793ef156360f9a54
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 22:17:23 +0800

    Update 003_std_atomic_long_long.cpp

--- cpp_11/003_std_atomic_long_long.cpp
+++ cpp_11/003_std_atomic_long_long.cpp
@@ -1,1 +1,30 @@
-1
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <array>
+ 
+std::atomic<long long> data{10};
+std::array<long long, 5> return_values{};
+ 
+void do_work(int thread_num)
+{
+    long long val = data.fetch_add(1, std::memory_order_relaxed);
+    return_values[thread_num] = val;
+}
+ 
+int main()
+{
+    {
+        std::jthread th0{do_work, 0};
+        std::jthread th1{do_work, 1};
+        std::jthread th2{do_work, 2};
+        std::jthread th3{do_work, 3};
+        std::jthread th4{do_work, 4};
+    }
+ 
+    std::cout << "Result : " << data << '\n';
+ 
+    for (long long val : return_values) {
+        std::cout << "Seen return value : " << val << std::endl;
+    }
+}
