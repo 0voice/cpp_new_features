c92c21056a6a89168306f5af3de6353f97ab971a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:26:34 +0800

    Update 003_template_default_template_parameters.cpp

--- cpp_11/003_template_default_template_parameters.cpp
+++ cpp_11/003_template_default_template_parameters.cpp
@@ -1,1 +1,10 @@
-1
+template <class T, class U = double>
+void f(T t = 0, U u = 0) {};
+void g()
+{
+    f(1, 'c'); // f<int, char>(1, 'c')
+    f(1);      // f<int, double>(1, 0), 使用了默认模板参数double
+    f();       // 错误: T无法被推导出来
+    f<int>();  // f<int, double>(0, 0), 使用了默认模板参数double
+    f<int, char>(); // f<int, char>(0, 0)
+}
