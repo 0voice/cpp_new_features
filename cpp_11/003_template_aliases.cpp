6e4644ee20cc3e5af1f3682d8c82b16bb6633ce5
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:23:54 +0800

    Update 003_template_aliases.cpp

--- cpp_11/003_template_aliases.cpp
+++ cpp_11/003_template_aliases.cpp
@@ -1,1 +1,13 @@
-1
+//gcc (GCC) 7.3.0
+#include<memory>
+#include<string>
+using namespace std;
+template<typename T>
+using ptr=std::shared_ptr<T>;
+ 
+int main()
+{
+    //这里借助模板别名，用ptr<string> 来代替std::shared_ptr<string>
+    ptr<string> p=std::make_shared<std::string>();
+    return 0;
+}
