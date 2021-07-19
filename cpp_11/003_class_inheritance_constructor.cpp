07420fe34e68791f216d535c1dfa814400d6cf08
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:15:21 +0800

    Update 003_class_inheritance_constructor.cpp

--- cpp_11/003_class_inheritance_constructor.cpp
+++ cpp_11/003_class_inheritance_constructor.cpp
@@ -1,1 +1,28 @@
-1
+#include <iostream>
+using namespace std;
+
+struct A
+{
+    A(int i = 10) : m_a(i)
+    {}
+
+    int m_a;
+};
+
+struct B : A
+{
+    using A::A;  // 继承构造函数
+
+    int m_b{ 100 };
+};
+
+int main()
+{
+    B b;
+    cout << b.m_a << endl; // 10
+    cout << b.m_b << endl; // 100
+
+    B bb(200);
+    cout << bb.m_a << endl; // 200
+    cout << bb.m_b << endl; // 100
+}
