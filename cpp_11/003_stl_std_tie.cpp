cfd4410f2f0591fffeb5e66779a1fbb27b050dd2
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:24:36 +0800

    Update 003_stl_std_tie.cpp

--- cpp_11/003_stl_std_tie.cpp
+++ cpp_11/003_stl_std_tie.cpp
@@ -1,1 +1,32 @@
-1
+#include <iostream>
+#include <string>
+#include <set>
+#include <tuple>
+ 
+struct S {
+    int n;
+    std::string s;
+    float d;
+    bool operator<(const S& rhs) const
+    {
+        // compares n to rhs.n,
+        // then s to rhs.s,
+        // then d to rhs.d
+        return std::tie(n, s, d) < std::tie(rhs.n, rhs.s, rhs.d);
+    }
+};
+ 
+int main()
+{
+    std::set<S> set_of_s; // S is LessThanComparable
+ 
+    S value{42, "Test", 3.14};
+    std::set<S>::iterator iter;
+    bool inserted;
+ 
+    // unpacks the return value of insert into iter and inserted
+    std::tie(iter, inserted) = set_of_s.insert(value);
+ 
+    if (inserted)
+        std::cout << "Value was inserted successfully\n";
+}
