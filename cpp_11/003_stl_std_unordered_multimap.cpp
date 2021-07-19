5908d0ff1805f35f21d51f28f19e99b1c1ffb59a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 20:17:15 +0800

    Update 003_stl_std_unordered_multimap.cpp

--- cpp_11/003_stl_std_unordered_multimap.cpp
+++ cpp_11/003_stl_std_unordered_multimap.cpp
@@ -1,1 +1,13 @@
-1
+#include <unordered_map>
+#include <iostream>
+#include <utility>
+ 
+int main()
+{
+    std::unordered_multimap<int, int> numbers;
+    std::cout << "Initially, numbers.empty(): " << numbers.empty() << '\n';
+ 
+    numbers.emplace(42, 13);
+    numbers.insert(std::make_pair(13317, 123)); 
+    std::cout << "After adding elements, numbers.empty(): " << numbers.empty() << '\n';
+}
