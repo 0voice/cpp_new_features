d58b40d74c00d493e3a74746b671465d8d79839d
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-19 21:14:32 +0800

    Update 003_class_delegating_constructors.cpp

--- cpp_11/003_class_delegating_constructors.cpp
+++ cpp_11/003_class_delegating_constructors.cpp
@@ -1,1 +1,16 @@
-1
+class A
+{
+    private:
+        int a;
+        int b;
+        char c;
+        char d;
+    public:
+        A(int num0,int num1,char C):a(num0),b(num1),c(C){}
+        A(int num0,char C):A(num0,0,C){}//b默认初始化为0
+        A(int num0):A(num0,'p'){b=1;}//b重新赋值为1
+        void getMembers()
+        {
+            cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+        }
+};
