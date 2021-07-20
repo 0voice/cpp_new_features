5f9a0e6b1c7cc0dcd6c2774856228dbf7206e97a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:05:00 +0800

    Update 003_std_thread_std_atomic.cpp

--- cpp_11/003_std_thread_std_atomic.cpp
+++ cpp_11/003_std_thread_std_atomic.cpp
@@ -1,1 +1,35 @@
-1
+#include <thread>
+#include <vector>
+#include <iostream>
+#include <atomic>
+ 
+std::atomic_flag lock = ATOMIC_FLAG_INIT;
+ 
+void f(int n)
+{
+    for (int cnt = 0; cnt < 40; ++cnt) {
+        while (lock.test_and_set(std::memory_order_acquire)) {  // acquire lock
+        // Since C++20, it is possible to update atomic_flag's
+        // value only when there is a chance to acquire the lock.
+        // See also: https://stackoverflow.com/questions/62318642
+        #if defined(__cpp_lib_atomic_flag_test)
+            while (lock.test(std::memory_order_relaxed))        // test lock
+        #endif
+                ; // spin
+        }
+        static int out{};
+        std::cout << n << ((++out % 40) == 0 ? '\n' : ' ');
+        lock.clear(std::memory_order_release);                  // release lock
+    }
+}
+ 
+int main()
+{
+    std::vector<std::thread> v;
+    for (int n = 0; n < 10; ++n) {
+        v.emplace_back(f, n);
+    }
+    for (auto& t : v) {
+        t.join();
+    }
+}
