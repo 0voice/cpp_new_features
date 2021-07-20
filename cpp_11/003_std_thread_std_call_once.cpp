6df05c1a480c9cac95d80421421f5176b4454b5b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:04:22 +0800

    Update 003_std_thread_std_call_once.cpp

--- cpp_11/003_std_thread_std_call_once.cpp
+++ cpp_11/003_std_thread_std_call_once.cpp
@@ -1,1 +1,49 @@
-1
+#include <iostream>
+#include <thread>
+#include <mutex>
+ 
+std::once_flag flag1, flag2;
+ 
+void simple_do_once()
+{
+    std::call_once(flag1, [](){ std::cout << "Simple example: called once\n"; });
+}
+ 
+void may_throw_function(bool do_throw)
+{
+  if (do_throw) {
+    std::cout << "throw: call_once will retry\n"; // this may appear more than once
+    throw std::exception();
+  }
+  std::cout << "Didn't throw, call_once will not attempt again\n"; // guaranteed once
+}
+ 
+void do_once(bool do_throw)
+{
+  try {
+    std::call_once(flag2, may_throw_function, do_throw);
+  }
+  catch (...) {
+  }
+}
+ 
+int main()
+{
+    std::thread st1(simple_do_once);
+    std::thread st2(simple_do_once);
+    std::thread st3(simple_do_once);
+    std::thread st4(simple_do_once);
+    st1.join();
+    st2.join();
+    st3.join();
+    st4.join();
+ 
+    std::thread t1(do_once, true);
+    std::thread t2(do_once, true);
+    std::thread t3(do_once, false);
+    std::thread t4(do_once, true);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+}
