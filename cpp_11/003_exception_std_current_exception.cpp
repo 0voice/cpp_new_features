127cbb84bb74cc197509bd73fb12f7dab0a473f8
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:11:57 +0800

    Update 003_exception_std_current_exception.cpp

--- cpp_11/003_exception_std_current_exception.cpp
+++ cpp_11/003_exception_std_current_exception.cpp
@@ -1,1 +1,26 @@
-1
+#include <iostream>
+#include <string>
+#include <exception>
+#include <stdexcept>
+ 
+void handle_eptr(std::exception_ptr eptr) // 按值传递 ok
+{
+    try {
+        if (eptr) {
+            std::rethrow_exception(eptr);
+        }
+    } catch(const std::exception& e) {
+        std::cout << "Caught exception \"" << e.what() << "\"\n";
+    }
+}
+ 
+int main()
+{
+    std::exception_ptr eptr;
+    try {
+        std::string().at(1); // 这生成一个 std::out_of_range
+    } catch(...) {
+        eptr = std::current_exception(); // 捕获
+    }
+    handle_eptr(eptr);
+} // std::out_of_range 的析构函数调用于此，在 ept 析构时
