33fcf85ba31154c10d34a8a08dd5e894b17bcb96
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:44:43 +0800

    Update 003_error_std_error_code.cpp

--- cpp_11/003_error_std_error_code.cpp
+++ cpp_11/003_error_std_error_code.cpp
@@ -1,13 +1,31 @@
-#include <thread>
 #include <iostream>
 #include <system_error>
- 
+
+enum class YourErrorCode {
+    kSuccess = 0,  // 别忘了 0 应该表示无错误
+    kNetworkError,
+    kBadRequest,
+    kServerError,
+};
+
+// 特化模版，启用对应的重载
+namespace std {
+template<>
+struct is_error_code_enum<YourErrorCode>: true_type {};
+}
+
+// 提供工厂函数
+// 工厂函数不必要写在 std 中
+std::error_code make_error_code(YourErrorCode code)
+{
+    return {
+        static_cast<int>(code),
+        std::generic_category(),  // 这里暂时用自带的 category
+    };
+}
+
 int main()
 {
-    try {
-        std::thread().detach(); // attempt to detach a non-thread
-    } catch(const std::system_error& e) {
-        std::cout << "Caught system_error with code " << e.code() 
-                  << " meaning " << e.what() << '\n';
-    }
+    std::error_code e = YourErrorCode::kBadRequest;
+    std::cout << e << '\n';  // 自带一个输出流的重载
 }
