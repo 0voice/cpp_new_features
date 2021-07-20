81da093296eadde17d8407bf2539bb64a7d9ad6b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:11:46 +0800

    Update 003_std_atomic_std_uint_least8_t.cpp

--- cpp_11/003_std_atomic_std_uint_least8_t.cpp
+++ cpp_11/003_std_atomic_std_uint_least8_t.cpp
@@ -1,1 +1,48 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_uint_least8_t uint_least8_t_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint_least8_t_tCount:%d\r\n",  uint_least8_t_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint_least8_t_tCount:%d\r\n",  uint_least8_t_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = uint_least8_t_tCount.load(memory_order_relaxed);
+    printf("finally uint_least8_t_tCount:%d\r\n",  x);
+}
+
+
+
+
