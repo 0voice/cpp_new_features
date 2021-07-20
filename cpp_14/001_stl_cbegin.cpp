4f9f62b5d8645ec1b141180ea1bf58058c520f10
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 20:11:30 +0800

    Update 001_stl_cbegin.cpp

--- cpp_14/001_stl_cbegin.cpp
+++ cpp_14/001_stl_cbegin.cpp
@@ -1,1 +1,15 @@
-1
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main ()
+{
+  set<string> myset= {"Java", "C++","SQL"};
+
+  // 显示内容:
+  for (auto it = myset.cbegin(); it != myset.cend(); ++it)
+    cout <<*it << '\n';
+    
+  return 0;
+}
