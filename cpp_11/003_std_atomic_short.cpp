e1ef558c05cf4359b723ed8118ee31cca6a42448
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:32:28 +0800

    Update 003_std_atomic_short.cpp

--- cpp_11/003_std_atomic_short.cpp
+++ cpp_11/003_std_atomic_short.cpp
@@ -1,1 +1,44 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_short shortCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("shortCount:%d\r\n",  shortCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("shortCount:%d\r\n",  shortCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = shortCount.load(memory_order_relaxed);
+    printf("finally shortCount:%d\r\n",  x);
+}
