e87241dab70058c448928165fcea3ffc68d45e29
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 19:57:42 +0800

    Update 001_iterator_make_reverse_iterator.cpp

--- cpp_14/001_iterator_make_reverse_iterator.cpp
+++ cpp_14/001_iterator_make_reverse_iterator.cpp
@@ -1,1 +1,18 @@
-1
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+ 
+int main() {
+    std::vector<int> v{ 1, 3, 10, 8, 22 };
+ 
+    std::sort(v.begin(), v.end());
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+ 
+    std::cout << '\n';
+ 
+    std::copy(
+        std::make_reverse_iterator(v.end()), 
+        std::make_reverse_iterator(v.begin()),
+        std::ostream_iterator<int>(std::cout, ", "));
+}
