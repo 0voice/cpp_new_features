6f6a9fdd9ec203fb8420c64696e94d2944e1f292
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:30:38 +0800

    Update 003_std_atomic_signed_char.cpp

--- cpp_11/003_std_atomic_signed_char.cpp
+++ cpp_11/003_std_atomic_signed_char.cpp
@@ -1,1 +1,44 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_schar scCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("cCount:%d\r\n",  scCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("scCount:%d\r\n",  scCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = scCount.load(memory_order_relaxed);
+    printf("finally scCount:%d\r\n",  x);
+}
