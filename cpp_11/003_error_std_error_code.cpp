6179e85c677982756187e8fe72a54aee32f472c3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:41:43 +0800

    Update 003_error_std_error_code.cpp

--- cpp_11/003_error_std_error_code.cpp
+++ cpp_11/003_error_std_error_code.cpp
@@ -1,1 +1,13 @@
-1
+#include <thread>
+#include <iostream>
+#include <system_error>
+ 
+int main()
+{
+    try {
+        std::thread().detach(); // attempt to detach a non-thread
+    } catch(const std::system_error& e) {
+        std::cout << "Caught system_error with code " << e.code() 
+                  << " meaning " << e.what() << '\n';
+    }
+}
