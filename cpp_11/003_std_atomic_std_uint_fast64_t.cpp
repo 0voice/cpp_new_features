6a785e3b60065a15badb1490e4d3df9d42fa65b7
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:21:23 +0800

    Update 003_std_atomic_std_uint_fast64_t.cpp

--- cpp_11/003_std_atomic_std_uint_fast64_t.cpp
+++ cpp_11/003_std_atomic_std_uint_fast64_t.cpp
@@ -1,1 +1,46 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_uint_fast64_t uint_fast64_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint_fast64_tCount:%d\r\n",  uint_fast64_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint_fast64_tCount:%d\r\n",  uint_fast64_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = uint_fast64_tCount.load(memory_order_relaxed);
+    printf("finally uint_fast64_tCount:%d\r\n",  x);
+}
+
+
