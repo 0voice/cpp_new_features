3afef062688f363b495c34033972b02573151b24
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:09:28 +0800

    Update 003_std_atomic_std_int64_t.cpp

--- cpp_11/003_std_atomic_std_int64_t.cpp
+++ cpp_11/003_std_atomic_std_int64_t.cpp
@@ -1,1 +1,47 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_int64_t int64_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int64_tCount:%d\r\n",  int64_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int64_tCount:%d\r\n",  int64_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = int64_tCount.load(memory_order_relaxed);
+    printf("finally int64_tCount:%d\r\n",  x);
+}
+
+
+
