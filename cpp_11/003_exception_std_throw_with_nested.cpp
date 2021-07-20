85f425d39f078f593053a7373c1f855b643cf356
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:14:23 +0800

    Update 003_exception_std_throw_with_nested.cpp

--- cpp_11/003_exception_std_throw_with_nested.cpp
+++ cpp_11/003_exception_std_throw_with_nested.cpp
@@ -1,1 +1,48 @@
-1
+#include <iostream>
+#include <stdexcept>
+#include <exception>
+#include <string>
+#include <fstream>
+ 
+// prints the explanatory string of an exception. If the exception is nested,
+// recurses to print the explanatory of the exception it holds
+void print_exception(const std::exception& e, int level =  0)
+{
+    std::cerr << std::string(level, ' ') << "exception: " << e.what() << '\n';
+    try {
+        std::rethrow_if_nested(e);
+    } catch(const std::exception& e) {
+        print_exception(e, level+1);
+    } catch(...) {}
+}
+ 
+// sample function that catches an exception and wraps it in a nested exception
+void open_file(const std::string& s)
+{
+    try {
+        std::ifstream file(s);
+        file.exceptions(std::ios_base::failbit);
+    } catch(...) {
+        std::throw_with_nested( std::runtime_error("Couldn't open " + s) );
+    }
+}
+ 
+// sample function that catches an exception and wraps it in a nested exception
+void run()
+{
+    try {
+        open_file("nonexistent.file");
+    } catch(...) {
+        std::throw_with_nested( std::runtime_error("run() failed") );
+    }
+}
+ 
+// runs the sample function above and prints the caught exception
+int main()
+{
+    try {
+        run();
+    } catch(const std::exception& e) {
+        print_exception(e);
+    }
+}
