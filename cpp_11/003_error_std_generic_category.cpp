e7e7f11237e0ea2ff848e65e4f170d1af2d6fd1e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:16:14 +0800

    Update 003_error_std_generic_category.cpp

--- cpp_11/003_error_std_generic_category.cpp
+++ cpp_11/003_error_std_generic_category.cpp
@@ -1,1 +1,11 @@
-1
+#include <iostream>
+#include <system_error>
+#include <cerrno>
+#include <string>
+int main()
+{
+    std::error_condition econd = std::generic_category().default_error_condition(EDOM);
+    std::cout << "Category: " << econd.category().name() << '\n'
+              << "Value: " << econd.value() << '\n'
+              << "Message: " << econd.message() << '\n';
+}
