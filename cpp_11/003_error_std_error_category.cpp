6c87bcfda8df141011f23c82bc4c2954502b9a95
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:15:48 +0800

    Update 003_error_std_error_category.cpp

--- cpp_11/003_error_std_error_category.cpp
+++ cpp_11/003_error_std_error_category.cpp
@@ -1,1 +1,17 @@
-1
+#include <iostream>
+#include <system_error>
+#include <iomanip>
+#include <string>
+ 
+int main()
+{
+    std::error_condition econd = std::system_category().default_error_condition(EDOM);
+    std::cout << "Category: " << econd.category().name() << '\n'
+              << "Value: " << econd.value() << '\n'
+              << "Message: " << econd.message() << '\n';
+ 
+    econd = std::system_category().default_error_condition(10001);
+    std::cout << "Category: " << econd.category().name() << '\n'
+              << "Value: " << econd.value() << '\n'
+              << "Message: " << econd.message() << '\n';
+}
