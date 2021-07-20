10b68b4682e02e862ea1e4d6856edf3aeed3f9ce
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:04:27 +0800

    Update 003_std_atomic_std_uint8_t.cpp

--- cpp_11/003_std_atomic_std_uint8_t.cpp
+++ cpp_11/003_std_atomic_std_uint8_t.cpp
@@ -1,1 +1,46 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_uint8_t uint8_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint8_tCount:%d\r\n",  uint8_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint8_tCount:%d\r\n",  uint8_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = uint8_tCount.load(memory_order_relaxed);
+    printf("finally uint8_tCount:%d\r\n",  x);
+}
+
+
