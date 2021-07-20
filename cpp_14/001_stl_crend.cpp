6bec1f0b57a152c41ec3904ec72119b7d7828313
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 20:13:46 +0800

    Update 001_stl_crend.cpp

--- cpp_14/001_stl_crend.cpp
+++ cpp_14/001_stl_crend.cpp
@@ -1,1 +1,25 @@
-1
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main() 
+{ 
+  
+    // initialize container 
+    map<int, int> mp; 
+  
+    // insert elements in random order 
+    mp.insert({ 2, 30 }); 
+    mp.insert({ 1, 40 }); 
+    mp.insert({ 3, 60 }); 
+    mp.insert({ 4, 20 }); 
+    mp.insert({ 5, 50 }); 
+  
+    // prints the elements 
+    cout << "\nThe map in reverse order is:\n"; 
+    cout << "KEY\tELEMENT\n"; 
+    for (auto itr = mp.crbegin(); itr != mp.crend(); ++itr) { 
+        cout << itr->first 
+             << '\t' << itr->second << '\n'; 
+    } 
+    return 0; 
+}
