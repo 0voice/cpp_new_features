6ce3ae0265658841a252fa2f07e18080cfdf3066
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:03:22 +0800

    Update 003_std_thread_std_mutex.cpp

--- cpp_11/003_std_thread_std_mutex.cpp
+++ cpp_11/003_std_thread_std_mutex.cpp
@@ -1,1 +1,32 @@
-1
+#include <iostream>
+#include <map>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <mutex>
+ 
+std::map<std::string, std::string> g_pages;
+std::mutex g_pages_mutex;
+ 
+void save_page(const std::string &url)
+{
+    // simulate a long page fetch
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::string result = "fake content";
+ 
+    std::lock_guard<std::mutex> guard(g_pages_mutex);
+    g_pages[url] = result;
+}
+ 
+int main() 
+{
+    std::thread t1(save_page, "http://foo");
+    std::thread t2(save_page, "http://bar");
+    t1.join();
+    t2.join();
+ 
+    // safe to access g_pages without lock now, as the threads are joined
+    for (const auto &pair : g_pages) {
+        std::cout << pair.first << " => " << pair.second << '\n';
+    }
+}
