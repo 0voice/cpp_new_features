95e79b7ed78e204bf78f594af51a5f92f04003b1
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:10:26 +0800

    Update 003_exception_std_exception_ptr.cpp

--- cpp_11/003_exception_std_exception_ptr.cpp
+++ cpp_11/003_exception_std_exception_ptr.cpp
@@ -1,1 +1,26 @@
-1
+#include <iostream>
+#include <string>
+#include <exception>
+#include <stdexcept>
+ 
+void handle_eptr(std::exception_ptr eptr) // passing by value is ok
+{
+    try {
+        if (eptr) {
+            std::rethrow_exception(eptr);
+        }
+    } catch(const std::exception& e) {
+        std::cout << "Caught exception \"" << e.what() << "\"\n";
+    }
+}
+ 
+int main()
+{
+    std::exception_ptr eptr;
+    try {
+        std::string().at(1); // this generates an std::out_of_range
+    } catch(...) {
+        eptr = std::current_exception(); // capture
+    }
+    handle_eptr(eptr);
+} // destructor for std::out_of_range called here, when the eptr is destructed
