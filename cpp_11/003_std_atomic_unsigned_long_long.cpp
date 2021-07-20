682da16738c06665e44cf2730174a308d18a11d9
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:38:04 +0800

    Update 003_std_atomic_unsigned_long_long.cpp

--- cpp_11/003_std_atomic_unsigned_long_long.cpp
+++ cpp_11/003_std_atomic_unsigned_long_long.cpp
@@ -1,1 +1,46 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_ullong ullCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("ullCount:%lld\r\n",  ullCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("ullCount:%lld\r\n",  ullCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = ullCount.load(memory_order_relaxed);
+    printf("finally ullCount:%ld\r\n",  x);
+}
+
+
