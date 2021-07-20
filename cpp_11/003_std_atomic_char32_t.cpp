60e5ff21b4cb6fc89636223fffeefd87819e7512
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:59:47 +0800

    Update 003_std_atomic_char32_t.cpp

--- cpp_11/003_std_atomic_char32_t.cpp
+++ cpp_11/003_std_atomic_char32_t.cpp
@@ -1,1 +1,48 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_char32_t char32_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("char32_tCount:%d\r\n",  char32_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("char32_tCount:%d\r\n",  char32_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = char32_tCount.load(memory_order_relaxed);
+    printf("finally char32_tCount:%d\r\n",  x);
+}
+
+
+
+
