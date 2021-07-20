c8b8c0ecb038fcfb8dd7d0725492f860e7c3782a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:19:58 +0800

    Update 003_std_atomic_std_int_fast32_t.cpp

--- cpp_11/003_std_atomic_std_int_fast32_t.cpp
+++ cpp_11/003_std_atomic_std_int_fast32_t.cpp
@@ -1,1 +1,44 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_int_fast32_t int_fast32_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int_fast32_tCount:%d\r\n",  int_fast32_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int_fast32_tCount:%d\r\n",  int_fast32_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = int_fast32_tCount.load(memory_order_relaxed);
+    printf("finally int_fast32_tCount:%d\r\n",  x);
+}
