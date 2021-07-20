20093697636e35df3bc72022ed5050d034d8e057
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 20:12:57 +0800

    Update 001_stl_rend.cpp

--- cpp_14/001_stl_rend.cpp
+++ cpp_14/001_stl_rend.cpp
@@ -1,1 +1,15 @@
-1
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main() 
+{ 
+    deque<char> dq = { 'a', 'b', 'c', 'd', 'e' }; 
+  
+    cout << "The deque in reverse order: "; 
+  
+    // prints the elements in reverse order 
+    for (auto it = dq.rbegin(); it != dq.rend(); ++it) 
+        cout << *it << " "; 
+  
+    return 0; 
+}
