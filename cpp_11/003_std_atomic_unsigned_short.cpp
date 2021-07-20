c2c2c497d87c6f7a9a99c1ae29f1bc7c8162612c
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:33:03 +0800

    Update 003_std_atomic_unsigned_short.cpp

--- cpp_11/003_std_atomic_unsigned_short.cpp
+++ cpp_11/003_std_atomic_unsigned_short.cpp
@@ -1,1 +1,44 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_ushort ushortCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("ushortCount:%d\r\n",  ushortCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("ushortCount:%d\r\n",  ushortCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = ushortCount.load(memory_order_relaxed);
+    printf("finally ushortCount:%d\r\n",  x);
+}
