27c0c2b5acaafe39d852bdd501078f8367247765
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:45:51 +0800

    Update 003_error_std_errc.cpp

--- cpp_11/003_error_std_errc.cpp
+++ cpp_11/003_error_std_errc.cpp
@@ -1,1 +1,15 @@
-1
+#include <iostream>
+#include <system_error>
+#include <thread>
+
+int main()
+{
+    try {
+        std::thread().detach(); // detaching a not-a-thread
+    } catch (const std::system_error& e) {
+        std::cout << "Caught a system_error\n";
+        if(e.code() == std::errc::invalid_argument)
+            std::cout << "The error condition is std::errc::invalid_argument\n";
+        std::cout << "the error description is " << e.what() << '\n';
+    }
+}
