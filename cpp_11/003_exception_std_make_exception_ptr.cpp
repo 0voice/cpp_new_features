b6de0db01a1553bc2d549f41f080a4a8a498669c
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:12:29 +0800

    Update 003_exception_std_make_exception_ptr.cpp

--- cpp_11/003_exception_std_make_exception_ptr.cpp
+++ cpp_11/003_exception_std_make_exception_ptr.cpp
@@ -1,1 +1,15 @@
-1
+// make_exception_ptr example
+#include <iostream>       // std::cout
+#include <exception>      // std::make_exception_ptr, std::rethrow_exception
+#include <stdexcept>      // std::logic_error
+
+int main () {
+  auto p = std::make_exception_ptr(std::logic_error("logic_error"));
+
+  try {
+     std::rethrow_exception (p);
+  } catch (const std::exception& e) {
+     std::cout << "exception caught: " << e.what() << '\n';
+  }
+  return 0;
+}
