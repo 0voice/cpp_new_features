e8c7417329a392e605f42ade3b1d886c03184551
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 19:55:05 +0800

    Update 001_initializer_list_rend.cpp

--- cpp_14/001_initializer_list_rend.cpp
+++ cpp_14/001_initializer_list_rend.cpp
@@ -1,1 +1,9 @@
-1
+#include <iostream>
+#include <iterator>
+ 
+int main() 
+{
+    auto il = { 3, 1, 4 };
+    for (auto it = std::rbegin(il); it != std::rend(il); ++it)
+        std::cout << *it << ' '; 
+}
