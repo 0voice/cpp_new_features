0c03a5cd8ea435788fdbf205151df03ca8f758d9
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:08:31 +0800

    Update 003_std_atomic_std_uint32_t.cpp

--- cpp_11/003_std_atomic_std_uint32_t.cpp
+++ cpp_11/003_std_atomic_std_uint32_t.cpp
@@ -1,1 +1,47 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_uint32_t uint32_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint32_tCount:%d\r\n",  uint32_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("uint32_tCount:%d\r\n",  uint32_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = uint32_tCount.load(memory_order_relaxed);
+    printf("finally uint32_tCount:%d\r\n",  x);
+}
+
+
+
