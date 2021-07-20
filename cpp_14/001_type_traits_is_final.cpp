ed2ec89a2158dff804ab0c3269de550a2d18cdf0
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 19:48:08 +0800

    Update 001_type_traits_is_final.cpp

--- cpp_14/001_type_traits_is_final.cpp
+++ cpp_14/001_type_traits_is_final.cpp
@@ -1,1 +1,13 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+class A {};
+class B final {};
+ 
+int main() 
+{
+    std::cout
+        << std::boolalpha
+        << std::is_final<A>::value << '\n'
+        << std::is_final<B>::value << '\n';
+}
