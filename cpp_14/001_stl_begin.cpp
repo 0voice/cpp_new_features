a6186cc004d41ab425735be67218a5f7ee574976
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 19:59:59 +0800

    Update 001_stl_begin.cpp

--- cpp_14/001_stl_begin.cpp
+++ cpp_14/001_stl_begin.cpp
@@ -1,1 +1,13 @@
-1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int ia[] = {0,1,2,3};
+	int *beg = begin(ia);
+	int *last = end(ia);
+	cout << *beg << endl;
+	cout << *(last-1) << endl;
+	return 0;
+}
