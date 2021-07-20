c622afe6005405c56b961d459a3134bf1680f2ec
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:54:07 +0800

    Update 001_type_traits_is_null_pointer.cpp

--- cpp_14/001_type_traits_is_null_pointer.cpp
+++ cpp_14/001_type_traits_is_null_pointer.cpp
@@ -1,1 +1,29 @@
-1
+#include <cstddef>
+#include <iostream>
+ 
+void f(int*)
+{
+   std::cout << "Pointer to integer overload\n";
+}
+ 
+void f(double*)
+{
+   std::cout << "Pointer to double overload\n";
+}
+ 
+void f(std::nullptr_t)
+{
+   std::cout << "null pointer overload\n";
+}
+ 
+int main()
+{
+    int* pi {}; double* pd {};
+ 
+    f(pi);
+    f(pd);
+    f(nullptr); // would be ambiguous without void f(nullptr_t)
+    // f(0);    // ambiguous call: all three functions are candidates
+    // f(NULL); // ambiguous if NULL is an integral null pointer constant 
+                // (as is the case in most implementations)
+}
