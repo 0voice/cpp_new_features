fcbce5929722732a4687228310167138905a05d2
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:58:31 +0800

    Update 003_std_atomic_char8_t.cpp

--- cpp_11/003_std_atomic_char8_t.cpp
+++ cpp_11/003_std_atomic_char8_t.cpp
@@ -1,1 +1,46 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_char8_t char8_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("char8_tCount:%d\r\n",  char8_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("char8_tCount:%d\r\n",  char8_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = char8_tCount.load(memory_order_relaxed);
+    printf("finally char8_tCount:%d\r\n",  x);
+}
+
+
