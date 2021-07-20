c203ab6cfdf9e36e3d82de380b0f9a6d1853c7be
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:59:12 +0800

    Update 003_std_atomic_char16_t.cpp

--- cpp_11/003_std_atomic_char16_t.cpp
+++ cpp_11/003_std_atomic_char16_t.cpp
@@ -1,1 +1,47 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_char16_t char16_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("char16_tCount:%d\r\n",  char16_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("char16_tCount:%d\r\n",  char16_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = char16_tCount.load(memory_order_relaxed);
+    printf("finally char16_tCount:%d\r\n",  x);
+}
+
+
+
