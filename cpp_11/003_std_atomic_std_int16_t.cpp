62a488ea946daea690e5b4233ca882b03c438189
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:05:14 +0800

    Update 003_std_atomic_std_int16_t.cpp

--- cpp_11/003_std_atomic_std_int16_t.cpp
+++ cpp_11/003_std_atomic_std_int16_t.cpp
@@ -1,1 +1,46 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_int16_t int16_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int16_tCount:%d\r\n",  int16_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int16_tCount:%d\r\n",  int16_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = int16_tCount.load(memory_order_relaxed);
+    printf("finally int16_tCount:%d\r\n",  x);
+}
+
+
