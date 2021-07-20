95f4487d00c9bcba868b4ebfadf5bd41fad4d4e4
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:02:52 +0800

    Update 003_std_thread_std_thread.cpp

--- cpp_11/003_std_thread_std_thread.cpp
+++ cpp_11/003_std_thread_std_thread.cpp
@@ -1,1 +1,30 @@
-1
+#include <iostream>
+#include <thread>
+#include <chrono>
+ 
+void foo()
+{
+    // simulate expensive operation
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+ 
+void bar()
+{
+    // simulate expensive operation
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+ 
+int main()
+{
+    std::cout << "starting first helper...\n";
+    std::thread helper1(foo);
+ 
+    std::cout << "starting second helper...\n";
+    std::thread helper2(bar);
+ 
+    std::cout << "waiting for helpers to finish..." << std::endl;
+    helper1.join();
+    helper2.join();
+ 
+    std::cout << "done!\n";
+}
