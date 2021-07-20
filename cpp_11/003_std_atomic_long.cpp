03a711970a36c3902b926d672e78b87fbf5f1d20
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 15:34:39 +0800

    Update 003_std_atomic_long.cpp

--- cpp_11/003_std_atomic_long.cpp
+++ cpp_11/003_std_atomic_long.cpp
@@ -1,1 +1,45 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_long longCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("longCount:%d\r\n",  longCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("longCount:%d\r\n",  longCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = longCount.load(memory_order_relaxed);
+    printf("finally longCount:%d\r\n",  x);
+}
+
