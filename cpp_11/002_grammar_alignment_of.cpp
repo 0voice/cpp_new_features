f5c6c677ef6356eca504f23cd326342956ffe04e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 17:51:38 +0800

    Update 002_grammar_alignment_of.cpp

--- cpp_11/002_grammar_alignment_of.cpp
+++ cpp_11/002_grammar_alignment_of.cpp
@@ -1,1 +1,12 @@
-1
+// alignment_of example
+#include <iostream>
+#include <type_traits>
+
+int main() {
+  std::cout << "alignment_of:" << std::endl;
+  std::cout << "char: " << std::alignment_of<char>::value << std::endl;
+  std::cout << "int: " << std::alignment_of<int>::value << std::endl;
+  std::cout << "int[20]: " << std::alignment_of<int[20]>::value << std::endl;
+  std::cout << "long long int: " << std::alignment_of<long long int>::value << std::endl;
+  return 0;
+}
