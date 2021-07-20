f25fc56d7c009e5a8ae5d0ea156be27be0d446ad
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-20 16:15:49 +0800

    Update 003_std_atomic_std_int_least64_t.cpp

--- cpp_11/003_std_atomic_std_int_least64_t.cpp
+++ cpp_11/003_std_atomic_std_int_least64_t.cpp
@@ -1,1 +1,50 @@
-1
+#include <thread>
+#include <atomic>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+atomic_int_least64_t int_least64_tCount(0);
+
+void threadfun1()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int_least64_tCount:%d\r\n",  int_least64_tCount);
+    }    
+}
+void threadfun2()
+{
+    for(int i =0; i< 1000; i++)
+    {
+        printf("int_least64_tCount:%d\r\n",  int_least64_tCount);
+    }    
+}
+
+int main()
+{
+    std::list<thread> lstThread;
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun1));
+    }
+    for (int i=0; i< 100; i++)
+    {
+        lstThread.push_back(thread(threadfun2));
+    }
+    
+    for (auto& th: lstThread)
+    {
+        th.join();
+    }
+
+    int x = int_least64_tCount.load(memory_order_relaxed);
+    printf("finally int_least64_tCount:%d\r\n",  x);
+}
+
+
+
+
+
+
