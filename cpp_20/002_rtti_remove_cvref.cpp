bda63150a58a89697d6170a9f62bbbbebbeed9b3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 16:05:42 +0800

    Update 002_rtti_remove_cvref.cpp

--- cpp_20/002_rtti_remove_cvref.cpp
+++ cpp_20/002_rtti_remove_cvref.cpp
@@ -1,1 +1,14 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+int main()
+{
+    std::cout << std::boolalpha
+              << std::is_same_v<std::remove_cvref_t<int>, int> << '\n'
+              << std::is_same_v<std::remove_cvref_t<int&>, int> << '\n'
+              << std::is_same_v<std::remove_cvref_t<int&&>, int> << '\n'
+              << std::is_same_v<std::remove_cvref_t<const int&>, int> << '\n'
+              << std::is_same_v<std::remove_cvref_t<const int[2]>, int[2]> << '\n'
+              << std::is_same_v<std::remove_cvref_t<const int(&)[2]>, int[2]> << '\n'
+              << std::is_same_v<std::remove_cvref_t<int(int)>, int(int)> << '\n';
+}
