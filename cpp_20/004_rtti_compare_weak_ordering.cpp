42cf9b8cd709f7a4516b20d4f278eaec133fde83
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 17:24:46 +0800

    Update 004_rtti_compare_weak_ordering.cpp

--- cpp_20/004_rtti_compare_weak_ordering.cpp
+++ cpp_20/004_rtti_compare_weak_ordering.cpp
@@ -1,1 +1,11 @@
-1
+// any type that is weakly ordered
+struct Weak {
+    bool operator==(Weak const&) const;
+    std::weak_ordering operator<=>(Weak const&) const;
+};
+
+struct Foo {
+    Weak w;
+    int i;
+    auto operator<=>(Foo const&) const = default;
+};
