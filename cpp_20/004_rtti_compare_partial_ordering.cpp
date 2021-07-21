f237eb9d90fa4a40ba422d186f4057d3ceb7849e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 17:22:56 +0800

    Update 004_rtti_compare_partial_ordering.cpp

--- cpp_20/004_rtti_compare_partial_ordering.cpp
+++ cpp_20/004_rtti_compare_partial_ordering.cpp
@@ -1,1 +1,23 @@
-1
+void test_compare() 
+{
+    struct ts {
+      int id;
+      float f;
+      std::partial_ordering operator<=>(const ts& that) const
+      {
+          if(id<0 || that.id<0)  return std::partial_ordering::unordered;
+          if(f>that.f) return std::partial_ordering::greater;
+          if(f<that.f) return std::partial_ordering::less;          
+          return std::partial_ordering::equivalent;
+      }
+      bool operator==(const ts& that) const
+      {
+          return (*this<=>that)==0;
+      }            
+    };
+    
+    ts ta = {2, 1.0f};  
+    ts  tb={3, 1.0f};
+    std::cout<< (ta==tb) <<std::endl;
+    std::cout<< (ta>tb) <<std::endl;
+}
