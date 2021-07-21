d97de84cd13ff1e4cb5e3e0c9d41c6a2f8b31000
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 16:03:07 +0800

    Update 002_rtti_is_pointer_interconvertible_with_class.cpp

--- cpp_20/002_rtti_is_pointer_interconvertible_with_class.cpp
+++ cpp_20/002_rtti_is_pointer_interconvertible_with_class.cpp
@@ -1,1 +1,30 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+struct A {
+    int m;
+};
+ 
+struct B {
+    B(B const&) {}
+};
+ 
+struct C {
+    virtual void foo();
+};
+ 
+struct D {
+    int m;
+ 
+    D(D const&) = default; // -> trivially copyable
+    D(int x): m(x+1) {}
+};
+ 
+int main()
+{
+    std::cout << std::boolalpha;
+    std::cout << std::is_trivially_copyable<A>::value << '\n';
+    std::cout << std::is_trivially_copyable<B>::value << '\n';
+    std::cout << std::is_trivially_copyable<C>::value << '\n';
+    std::cout << std::is_trivially_copyable<D>::value << '\n';
+}
