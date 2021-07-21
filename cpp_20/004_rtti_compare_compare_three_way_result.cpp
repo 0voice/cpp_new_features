188462b1493741ce56db5d2f01585217143000a1
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 17:45:39 +0800

    Update 004_rtti_compare_compare_three_way_result.cpp

--- cpp_20/004_rtti_compare_compare_three_way_result.cpp
+++ cpp_20/004_rtti_compare_compare_three_way_result.cpp
@@ -1,1 +1,22 @@
-1
+#include <compare>
+#include <type_traits>
+#include <iostream>
+ 
+template <class Ord>
+void print_cmp_type()
+{
+    if constexpr (std::is_same_v<Ord, std::strong_ordering>)
+        std::cout << "strong ordering\n";
+    else if constexpr (std::is_same_v<Ord, std::weak_ordering>)
+        std::cout << "weak ordering\n";
+    else if constexpr (std::is_same_v<Ord, std::partial_ordering>)
+        std::cout << "partial ordering\n";
+    else
+        std::cout << "illegal comparison result type\n";
+}
+ 
+int main()
+{
+    print_cmp_type<std::compare_three_way_result_t<int>>();
+    print_cmp_type<std::compare_three_way_result_t<double>>();
+}
