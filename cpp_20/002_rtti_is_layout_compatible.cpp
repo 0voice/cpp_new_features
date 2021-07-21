37380024119be16a25518a1f6d2616190cf833f0
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 15:49:58 +0800

    Update 002_rtti_is_layout_compatible.cpp

--- cpp_20/002_rtti_is_layout_compatible.cpp
+++ cpp_20/002_rtti_is_layout_compatible.cpp
@@ -1,1 +1,28 @@
-1
+#include <type_traits>
+#include <iostream>
+ 
+struct Foo {
+    int x;
+    char y;
+};
+ 
+class Bar {
+    const int u = 42;
+    volatile char v = '*';
+};
+ 
+enum E0 : int {};
+enum class E1 : int {};
+ 
+int main()
+{
+    std::cout << std::boolalpha
+        << std::is_layout_compatible_v<const void, volatile void> << '\n'
+        << std::is_layout_compatible_v<Foo, Bar> << '\n'
+        << std::is_layout_compatible_v<Foo[2], Bar[2]> << '\n'
+        << std::is_layout_compatible_v<int, E0> << '\n'
+        << std::is_layout_compatible_v<E0, E1> << '\n'
+        << std::is_layout_compatible_v<long, unsigned long> << '\n'
+        << std::is_layout_compatible_v<char*, const char*> << '\n'
+        << std::is_layout_compatible_v<char*, char* const> << '\n';
+}
