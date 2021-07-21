1fe537fe0921fe34278511c4d4d0d5951d30b4c8
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 16:03:45 +0800

    Update 002_rtti_is_corresponding_member.cpp

--- cpp_20/002_rtti_is_corresponding_member.cpp
+++ cpp_20/002_rtti_is_corresponding_member.cpp
@@ -1,1 +1,17 @@
-1
+#include <type_traits>
+#include <iostream>
+ 
+struct Foo { int x; };
+struct Bar { int y; double z; };
+ 
+struct Baz : Foo, Bar {}; // not standard-layout
+ 
+int main()
+{
+    std::cout << std::boolalpha
+        << std::is_same_v<decltype(&Baz::x), int Foo::*> << '\n'
+        << std::is_same_v<decltype(&Baz::y), int Bar::*> << '\n'
+        << std::is_corresponding_member(&Foo::x, &Bar::y) << '\n'
+        << std::is_corresponding_member(&Baz::x, &Baz::y) << '\n'
+        << std::is_corresponding_member<Baz, Baz>(&Baz::x, &Baz::y) << '\n';
+}
