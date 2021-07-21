23ad154934aec030fc25812dcb109ca9bc7a0715
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 17:13:56 +0800

    Update 003_rtti_suspend_always.cpp

--- cpp_20/003_rtti_suspend_always.cpp
+++ cpp_20/003_rtti_suspend_always.cpp
@@ -1,1 +1,13 @@
-1
+struct promise_type {
+    int current_value;
+    static wrapper get_return_object_on_allocation_failure() noexcept { return wrapper{nullptr}; }
+    wrapper get_return_object() { return wrapper{handle::from_promise(*this)}; }
+    auto initial_suspend() { return std::suspend_always{}; }
+    auto final_suspend() noexcept { return std::suspend_always{}; }
+    void unhandled_exception() { std::terminate(); }
+    void return_void() {}
+    auto yield_value(int value) {
+        current_value = value;
+        return std::suspend_always{};
+    }
+};
