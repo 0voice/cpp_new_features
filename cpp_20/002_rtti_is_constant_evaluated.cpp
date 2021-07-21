fbedf14ff6e5b40b52f295bb84421e081fcae214
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 16:22:13 +0800

    Update 002_rtti_is_constant_evaluated.cpp

--- cpp_20/002_rtti_is_constant_evaluated.cpp
+++ cpp_20/002_rtti_is_constant_evaluated.cpp
@@ -1,1 +1,14 @@
-1
+#include <type_traits>
+
+int foo(int arg) {
+    if (std::is_constant_evaluated()) {
+        return 1;
+    } else {
+        return 0;
+    }
+}  
+
+int main() {
+    const auto b = foo(0);
+    return b;
+}
