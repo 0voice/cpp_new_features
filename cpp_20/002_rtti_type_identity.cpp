ce7bf51101eb8e02d6ec83d2297466ac0ef9c60f
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-21 16:18:08 +0800

    Update 002_rtti_type_identity.cpp

--- cpp_20/002_rtti_type_identity.cpp
+++ cpp_20/002_rtti_type_identity.cpp
@@ -1,1 +1,17 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+template <class T>
+T foo(T a, T b) {
+  return a + b;
+}
+ 
+template <class T>
+T bar(T a, std::type_identity_t<T> b) {
+  return a + b;
+}
+ 
+int main() {
+  // foo(4.2, 1); // error, deduced conflicting types for 'T'
+  std::cout << bar(4.2, 1) << '\n';  // OK, calls bar<double>
+}
