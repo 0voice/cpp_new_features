de75779e9275b40401291fc59f097bb6fc7538e3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:51:42 +0800

    Update 004_tuple_make_from_tuple.cpp

--- cpp_17/004_tuple_make_from_tuple.cpp
+++ cpp_17/004_tuple_make_from_tuple.cpp
@@ -1,1 +1,14 @@
-1
+#include <iostream>
+#include <tuple>
+ 
+struct Foo {
+    Foo(int first, float second, int third) {
+        std::cout << first << ", " << second << ", " << third << "\n";
+    }
+};
+ 
+int main()
+{
+   auto tuple = std::make_tuple(42, 3.14f, 0);
+   std::make_from_tuple<Foo>(std::move(tuple));
+}
