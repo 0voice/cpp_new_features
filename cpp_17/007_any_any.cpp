bdedddb4c12cddce733eeab9d6b21451a901ceb8
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:39:30 +0800

    Update 007_any_any.cpp

--- cpp_17/007_any_any.cpp
+++ cpp_17/007_any_any.cpp
@@ -1,1 +1,45 @@
-1
+#include <any>
+#include <iostream>
+ 
+int main()
+{
+    std::cout << std::boolalpha;
+ 
+    // any type
+    std::any a = 1;
+    std::cout << a.type().name() << ": " << std::any_cast<int>(a) << '\n';
+    a = 3.14;
+    std::cout << a.type().name() << ": " << std::any_cast<double>(a) << '\n';
+    a = true;
+    std::cout << a.type().name() << ": " << std::any_cast<bool>(a) << '\n';
+ 
+    // bad cast
+    try
+    {
+        a = 1;
+        std::cout << std::any_cast<float>(a) << '\n';
+    }
+    catch (const std::bad_any_cast& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+ 
+    // has value
+    a = 1;
+    if (a.has_value())
+    {
+        std::cout << a.type().name() << '\n';
+    }
+ 
+    // reset
+    a.reset();
+    if (!a.has_value())
+    {
+        std::cout << "no value\n";
+    }
+ 
+    // pointer to contained data
+    a = 1;
+    int* i = std::any_cast<int>(&a);
+    std::cout << *i << "\n";
+}
