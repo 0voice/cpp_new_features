61343392a57481944ebded6a20c4454a2c242868
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:52:49 +0800

    Update 009_initializer_list_data.cpp

--- cpp_17/009_initializer_list_data.cpp
+++ cpp_17/009_initializer_list_data.cpp
@@ -1,1 +1,14 @@
-1
+#include <string>
+#include <cstring>
+#include <iostream>
+ 
+int main()
+{
+    std::string s {"Hello world!\n"};
+ 
+    char a[20]; // storage for a C-style string
+    std::strcpy(a, std::data(s));
+    // [s.data(), s.data() + s.size()] is guaranteed to be an NTBS since C++11
+ 
+    std::cout << a;
+}
