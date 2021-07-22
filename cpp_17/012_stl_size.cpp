54d06493889ec0160fb778f78ebbc5211272f466
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:59:47 +0800

    Update 012_stl_size.cpp

--- cpp_17/012_stl_size.cpp
+++ cpp_17/012_stl_size.cpp
@@ -1,1 +1,19 @@
-1
+#include <iostream>
+#include <vector>
+#include <iterator>
+ 
+int main() 
+{
+    std::vector<int> v = { 3, 1, 4 };
+    std::cout << std::size(v) << '\n'; 
+ 
+    int a[] = { -5, 10, 15 };
+    std::cout << std::size(a) << '\n';
+ 
+    // since C++20 the signed size (ssize) can avail
+    auto i = std::ssize(v);
+    for (--i; i != -1; --i) {
+        std::cout << v[i] << (i ? ' ' : '\n');
+    }
+    std::cout << "i = " << i << '\n';
+}
