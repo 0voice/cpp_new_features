6a5a0b553c431aaa0d93a99418d3c97c9f4467c4
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:08:31 +0800

    Update 002_type_traits_is_nothrow_swappable_with.cpp

--- cpp_17/002_type_traits_is_nothrow_swappable_with.cpp
+++ cpp_17/002_type_traits_is_nothrow_swappable_with.cpp
@@ -1,1 +1,39 @@
-1
+#include <type_traits>
+#include <iostream>
+
+struct A
+{
+    A(const A&);
+};
+
+struct B
+{
+};
+
+void swap(B&, B&);
+
+struct C
+{
+};
+
+void swap(C&, C&) noexcept;
+
+struct D
+{
+    D(const D&) noexcept;
+    D& operator=(const D&) noexcept;
+};
+
+int main()
+{
+    std::cout << "std::__is_nothrow_swappable<int>::value = "
+              << std::__is_nothrow_swappable<int>::value << '\n';
+    std::cout << "std::__is_nothrow_swappable<A>::value = "
+              << std::__is_nothrow_swappable<A>::value << '\n';
+    std::cout << "std::__is_nothrow_swappable<B>::value = "
+              << std::__is_nothrow_swappable<B>::value << '\n';
+    std::cout << "std::__is_nothrow_swappable<C>::value = "
+              << std::__is_nothrow_swappable<C>::value << '\n';
+    std::cout << "std::__is_nothrow_swappable<D>::value = "
+              << std::__is_nothrow_swappable<D>::value << '\n';
+}
