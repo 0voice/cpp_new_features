9e58411cb410cd46a6e639809fe21db73a017855
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:23:13 +0800

    Update 006_variant_holds_alternative.cpp

--- cpp_17/006_variant_holds_alternative.cpp
+++ cpp_17/006_variant_holds_alternative.cpp
@@ -1,1 +1,12 @@
-1
+#include <variant>
+#include <string>
+#include <iostream>
+int main()
+{
+    std::variant<int, std::string> v = "abc";
+    std::cout << std::boolalpha
+              << "variant holds int? "
+              << std::holds_alternative<int>(v) << '\n'
+              << "variant holds string? "
+              << std::holds_alternative<std::string>(v) << '\n';
+}
