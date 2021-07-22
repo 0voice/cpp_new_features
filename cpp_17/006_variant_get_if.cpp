186e2bcef1d5418295cbb300a800a37bc5818f3e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:24:23 +0800

    Update 006_variant_get_if.cpp

--- cpp_17/006_variant_get_if.cpp
+++ cpp_17/006_variant_get_if.cpp
@@ -1,1 +1,12 @@
-1
+#include <variant>
+#include <iostream>
+ 
+int main()
+{
+    std::variant<int, float> v{12};
+ 
+    if(auto pval = std::get_if<int>(&v))
+      std::cout << "variant value: " << *pval << '\n'; 
+    else 
+      std::cout << "failed to get value!" << '\n'; 
+}
