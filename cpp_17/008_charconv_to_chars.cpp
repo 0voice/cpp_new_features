912c852762682bf1ed9ba4b1d616816c5eadd86e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:45:20 +0800

    Update 008_charconv_to_chars.cpp

--- cpp_17/008_charconv_to_chars.cpp
+++ cpp_17/008_charconv_to_chars.cpp
@@ -1,1 +1,16 @@
-1
+#include <iostream>
+#include <charconv>
+#include <system_error>
+#include <string_view>
+#include <array>
+ 
+int main()
+{
+    std::array<char, 10> str;
+ 
+    if(auto [ptr, ec] = std::to_chars(str.data(), str.data() + str.size(), 42);
+       ec == std::errc())
+         std::cout << std::string_view
+           (str.data(), ptr);              // C++20, uses string_view(first, last)
+      //   (str.data(), ptr - str.data()); // C++17, uses string_view(ptr, length)
+}
