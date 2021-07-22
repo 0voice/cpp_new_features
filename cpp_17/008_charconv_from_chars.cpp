467765fa8a9e1098f7a422d49c01ed9dd4f0a4f3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:46:38 +0800

    Update 008_charconv_from_chars.cpp

--- cpp_17/008_charconv_from_chars.cpp
+++ cpp_17/008_charconv_from_chars.cpp
@@ -1,1 +1,18 @@
-1
+#include <array>
+#include <charconv>
+#include <iostream>
+#include <string_view>
+ 
+int main()
+{
+    std::array<char, 10> str{"42 xyz "};
+    int result;
+    if(auto [p, ec] = std::from_chars(str.data(), str.data()+str.size(), result);
+       ec == std::errc())
+        std::cout << result << "\n" "p -> \"" << p << "\"\n";
+ 
+    std::string_view sv{"24 abc "};
+    if(auto [p, ec] = std::from_chars(data(sv), data(sv)+size(sv), result);
+       ec == std::errc())
+        std::cout << result << "\n" "p -> \"" << p << "\"\n";
+}
