39fd5983664a53db5431b898b0eea4690bfd9ee3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:29:31 +0800

    Update 002_type_traits_disjunction.cpp

--- cpp_17/002_type_traits_disjunction.cpp
+++ cpp_17/002_type_traits_disjunction.cpp
@@ -1,1 +1,16 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+using result0 =
+    std::disjunction<std::bool_constant<false>, std::bool_constant<false>,
+                     std::bool_constant<false>>;
+using result1 =
+    std::disjunction<std::bool_constant<true>, std::bool_constant<false>,
+                     std::bool_constant<false>>;
+ 
+int main()
+{
+    std::cout << std::boolalpha;
+    std::cout << result0::value << '\n';
+    std::cout << result1::value << '\n';
+}
