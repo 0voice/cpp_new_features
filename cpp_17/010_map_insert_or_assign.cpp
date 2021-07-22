ef90e2863c4e8a47dcf5e0d1b860228424c949a3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:54:12 +0800

    Update 010_map_insert_or_assign.cpp

--- cpp_17/010_map_insert_or_assign.cpp
+++ cpp_17/010_map_insert_or_assign.cpp
@@ -1,1 +1,24 @@
-1
+#include <iostream>
+#include <map>
+#include <string>
+ 
+auto print_node = [](const auto &node) {
+    std::cout << "[" << node.first << "] = " << node.second << '\n';
+};
+ 
+auto print_result = [](auto const &pair) {
+    std::cout << (pair.second ? "inserted: " : "assigned: ");
+    print_node(*pair.first);
+};
+ 
+int main()
+{
+    std::map<std::string, std::string> myMap;
+ 
+    print_result( myMap.insert_or_assign("a", "apple"     ) );
+    print_result( myMap.insert_or_assign("b", "banana"    ) );
+    print_result( myMap.insert_or_assign("c", "cherry"    ) );
+    print_result( myMap.insert_or_assign("c", "clementine") );
+ 
+    for (const auto &node : myMap) { print_node(node); }
+}
