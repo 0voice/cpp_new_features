5690f863509efcbbac77c5390ade9ae8eb736ece
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:28:15 +0800

    Update 002_type_traits_void_t.cpp

--- cpp_17/002_type_traits_void_t.cpp
+++ cpp_17/002_type_traits_void_t.cpp
@@ -1,1 +1,22 @@
-1
+struct NoInnerType
+{
+	int m_i;
+};
+ 
+struct HaveInnerType
+{
+	using type = int; //类型别名
+	void myfunc() {}
+};
+ 
+ 
+//泛化版本
+template <typename T,typename U = std::void_t<> >
+struct HasTypeMem : std::false_type  //struct 默认是public ,class默认是private继承
+{
+};
+//特化版本
+template <typename T>
+struct HasTypeMem<T, std::void_t<typename T::type> > : std::true_type 
+{
+};
