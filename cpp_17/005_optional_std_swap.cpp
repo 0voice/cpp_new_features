b8dcf1f23692e38c96326ef25f287684e9fcf9db
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:54:28 +0800

    Update 005_optional_std_swap.cpp

--- cpp_17/005_optional_std_swap.cpp
+++ cpp_17/005_optional_std_swap.cpp
@@ -1,1 +1,15 @@
-1
+#include <algorithm>
+#include <iostream>
+ 
+int main()
+{
+    int a = 5, b = 3;
+ 
+    // before
+    std::cout << a << ' ' << b << '\n';
+ 
+    std::swap(a,b);
+ 
+    // after
+    std::cout << a << ' ' << b << '\n';
+}
