eb1ee8ca54f9f79504a4d256943be2ece76b73cf
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:38:39 +0800

    Update 006_variant_variant_npos.cpp

--- cpp_17/006_variant_variant_npos.cpp
+++ cpp_17/006_variant_variant_npos.cpp
@@ -1,1 +1,27 @@
-1
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <variant>
+ 
+struct Demon
+{
+    Demon(int) {}
+    Demon(const Demon&) { throw std::domain_error("copy ctor"); }
+    Demon& operator= (const Demon&) = default;
+};
+ 
+int main()
+{
+    std::variant<int, Demon> var{42};
+    std::cout
+        << std::boolalpha
+        << "index == npos: " << (var.index() == std::variant_npos) << '\n';
+ 
+    try { var = Demon{666}; } catch (const std::domain_error& ex)
+    {
+        std::cout
+            << "Exception: " << ex.what() << '\n'
+            << "index == npos: " << (var.index() == std::variant_npos) << '\n'
+            << "valueless: " << var.valueless_by_exception() << '\n';
+    }
+}
