490505e4c598978cae136242d85359ef7f04fcce
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:21:08 +0800

    Update 006_variant_variant.cpp

--- cpp_17/006_variant_variant.cpp
+++ cpp_17/006_variant_variant.cpp
@@ -1,1 +1,37 @@
-1
+#include <variant>
+#include <string>
+#include <cassert>
+#include <iostream>
+ 
+int main()
+{
+    std::variant<int, float> v, w;
+    v = 42; // v contains int
+    int i = std::get<int>(v);
+    assert(42 == i); // succeeds
+    w = std::get<int>(v);
+    w = std::get<0>(v); // same effect as the previous line
+    w = v; // same effect as the previous line
+ 
+//  std::get<double>(v); // error: no double in [int, float]
+//  std::get<3>(v);      // error: valid index values are 0 and 1
+ 
+    try {
+      std::get<float>(w); // w contains int, not float: will throw
+    }
+    catch (const std::bad_variant_access& ex) {
+        std::cout << ex.what() << '\n';
+    }
+ 
+    using namespace std::literals;
+ 
+    std::variant<std::string> x("abc");
+    // converting constructors work when unambiguous
+    x = "def"; // converting assignment also works when unambiguous
+ 
+    std::variant<std::string, void const*> y("abc");
+    // casts to void const * when passed a char const *
+    assert(std::holds_alternative<void const*>(y)); // succeeds
+    y = "xyz"s;
+    assert(std::holds_alternative<std::string>(y)); // succeeds
+}
