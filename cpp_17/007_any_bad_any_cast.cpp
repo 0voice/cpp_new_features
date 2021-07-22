92d94eaca8f434342fadb8944f3a1c6dbb9f6fd2
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:44:39 +0800

    Update 007_any_bad_any_cast.cpp

--- cpp_17/007_any_bad_any_cast.cpp
+++ cpp_17/007_any_bad_any_cast.cpp
@@ -1,1 +1,16 @@
-1
+#include <iostream>
+#include <typeinfo>
+ 
+struct Foo { virtual ~Foo() {} };
+struct Bar { virtual ~Bar() {} };
+ 
+int main()
+{
+    Bar b;
+    try {
+        Foo& f = dynamic_cast<Foo&>(b);
+    } catch(const std::bad_cast& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+}
