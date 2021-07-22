3bec838f40e1bc402f6a6b6cefa48d02784887f3
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:27:00 +0800

    Update 006_variant_monostate.cpp

--- cpp_17/006_variant_monostate.cpp
+++ cpp_17/006_variant_monostate.cpp
@@ -1,1 +1,29 @@
-1
+#include <variant>
+#include <iostream>
+#include <cassert>
+ 
+struct S
+{
+    S(int i) : i(i) {}
+    int i;
+};
+ 
+int main() {
+ 
+    // Without the monostate type this declaration will fail.
+    // This is because S is not default-constructible.
+ 
+    std::variant<std::monostate, S> var; 
+    assert(var.index() == 0);
+ 
+    try {
+        std::get<S>(var); // throws! We need to assign a value
+    }
+    catch(const std::bad_variant_access& e) {
+        std::cout << e.what() << '\n';
+    }
+ 
+    var = 12; 
+ 
+    std::cout << std::get<S>(var).i << '\n';
+}
