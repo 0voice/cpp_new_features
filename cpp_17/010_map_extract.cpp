7f4f2b4130af678c1064170d9b34e4639a7f2393
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 22:03:14 +0800

    Update 010_map_extract.cpp

--- cpp_17/010_map_extract.cpp
+++ cpp_17/010_map_extract.cpp
@@ -1,11 +1,19 @@
-// clang++ -std=c++17 -Wall -pedantic main.cpp
-#include<map>
-#include<string>
-#include<algorithm>
-
-int main() {
-  std::map<int, std::string> m{ {10, "potato"}, {1, "banana"} };
-  auto nodeHandler = m.extract(10);
-  nodeHandler.key() = 2;
-  m.insert(std::move(nodeHandler)); // { { 1, "banana" }, { 2, "potato" } }
+#include <iostream>
+#include <utility>
+#include <string>
+ 
+#include <unordered_map>
+int main()
+{
+    using namespace std::literals;
+    std::unordered_map<std::string, std::string> m;
+ 
+    m.try_emplace("a", "a"s);
+    m.try_emplace("b", "abcd");
+    m.try_emplace("c", 10, 'c');
+    m.try_emplace("c", "Won't be inserted");
+ 
+    for (const auto &p : m) {
+        std::cout << p.first << " => " << p.second << '\n';
+    }
 }
