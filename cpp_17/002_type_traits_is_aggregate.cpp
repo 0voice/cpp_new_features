67a15bcbb0034811870510c856ec814eec85ee85
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 19:52:34 +0800

    Update 002_type_traits_is_aggregate.cpp

--- cpp_17/002_type_traits_is_aggregate.cpp
+++ cpp_17/002_type_traits_is_aggregate.cpp
@@ -1,1 +1,18 @@
-1
+#include <iostream>
+#include <string>
+#include <complex>
+ 
+template<typename T>
+struct D : std::string, std::complex<T>
+{
+	std::string data;
+};
+ 
+int main(void)
+{
+	D<float> s{ {"hello"}, {4.5,6.7}, "world" }; // OK since C++17
+	const auto result = std::is_aggregate<decltype(s)>::value;
+	std::cout << result << std::endl;
+ 
+	return 0;
+}
