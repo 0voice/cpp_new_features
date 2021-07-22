e2be815e3127391408b6966238b125da86fe080c
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:27:47 +0800

    Update 006_variant_bad_variant_access.cpp

--- cpp_17/006_variant_bad_variant_access.cpp
+++ cpp_17/006_variant_bad_variant_access.cpp
@@ -1,1 +1,14 @@
-1
+#include <variant>
+#include <iostream>
+ 
+int main()
+{
+    std::variant<int, float> v;
+    v = 12;
+    try {
+      std::get<float>(v);
+    }
+    catch(const std::bad_variant_access& e) {
+        std::cout << e.what() << '\n';
+    }
+}
