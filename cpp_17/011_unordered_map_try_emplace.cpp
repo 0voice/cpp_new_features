88ebafea431b639144beddc609b466016e799c4a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:59:09 +0800

    Update 011_unordered_map_try_emplace.cpp

--- cpp_17/011_unordered_map_try_emplace.cpp
+++ cpp_17/011_unordered_map_try_emplace.cpp
@@ -1,1 +1,19 @@
-1
+#include <iostream>
+#include <utility>
+#include <string>
+ 
+#include <unordered_map>
+int main()
+{
+    using namespace std::literals;
+    std::unordered_map<std::string, std::string> m;
+ 
+    m.try_emplace("a", "a"s);
+    m.try_emplace("b", "abcd");
+    m.try_emplace("c", 10, 'c');
+    m.try_emplace("c", "Won't be inserted");
+ 
+    for (const auto &p : m) {
+        std::cout << p.first << " => " << p.second << '\n';
+    }
+}
