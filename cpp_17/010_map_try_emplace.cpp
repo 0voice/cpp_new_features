606d23d8abba58ac09f480e58bc7053e8c29400f
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:54:53 +0800

    Update 010_map_try_emplace.cpp

--- cpp_17/010_map_try_emplace.cpp
+++ cpp_17/010_map_try_emplace.cpp
@@ -1,1 +1,18 @@
-1
+#include <iostream>
+#include <utility>
+#include <string>
+ 
+#include <map>
+int main()
+{
+    using namespace std::literals;
+    std::map<std::string, std::string> m;
+ 
+    m.try_emplace("a", "a"s);
+    m.try_emplace("b", "abcd");
+    m.try_emplace("c", 10, 'c');
+ 
+    for (const auto &p : m) {
+        std::cout << p.first << " => " << p.second << '\n';
+    }
+}
