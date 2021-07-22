051ff84edc0d556df81aba630dc4907340aba06d
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 22:03:38 +0800

    Update 010_map_merge.cpp

--- cpp_17/010_map_merge.cpp
+++ cpp_17/010_map_merge.cpp
@@ -1,41 +1,17 @@
+#include <map>
 #include <iostream>
-#include <iterator>
-#include <algorithm>
-#include <vector>
-#include <random>
-#include <functional>
+#include <string>
  
 int main()
 {
-    // fill the vectors with random numbers
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_int_distribution<> dis(0, 9);
- 
-    std::vector<int> v1(10), v2(10);
-    std::generate(v1.begin(), v1.end(), std::bind(dis, std::ref(mt)));
-    std::generate(v2.begin(), v2.end(), std::bind(dis, std::ref(mt)));
- 
-    // sort
-    std::sort(v1.begin(), v1.end());
-    std::sort(v2.begin(), v2.end());
- 
-    // output v1
-    std::cout << "v1 : ";
-    std::copy(v1.begin(), v1.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
- 
-    // output v2
-    std::cout << "v2 : ";
-    std::copy(v2.begin(), v2.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
- 
-    // merge
-    std::vector<int> dst;
-    std::merge(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(dst));
- 
-    // output
-    std::cout << "dst: ";
-    std::copy(dst.begin(), dst.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
+  std::map<int, std::string> ma {{1, "apple"}, {5, "pear"}, {10, "banana"}};
+  std::map<int, std::string> mb {{2, "zorro"}, {4, "batman"}, {5, "X"}, {8, "alpaca"}};
+  std::map<int, std::string> u;
+  u.merge(ma);
+  std::cout << "ma.size(): " << ma.size() << '\n';
+  u.merge(mb);
+  std::cout << "mb.size(): " << mb.size() << '\n';
+  std::cout << "mb.at(5): " << mb.at(5) << '\n';
+  for(auto const &kv: u)
+    std::cout << kv.first << ", " << kv.second << '\n';
 }
