55958618c7afa7ac9d3a35a977a66b7aafbf7992
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:29:07 +0800

    Update 006_variant_variant_size_v.cpp

--- cpp_17/006_variant_variant_size_v.cpp
+++ cpp_17/006_variant_variant_size_v.cpp
@@ -1,1 +1,16 @@
-1
+#include <any>
+#include <cstdio>
+#include <variant>
+ 
+static_assert(std::variant_size_v<std::variant<>> == 0);
+static_assert(std::variant_size_v<std::variant<int>> == 1);
+static_assert(std::variant_size_v<std::variant<int, int>> == 2);
+static_assert(std::variant_size_v<std::variant<int, int, int>> == 3);
+static_assert(std::variant_size_v<std::variant<int, float, double>> == 3);
+static_assert(std::variant_size_v<std::variant<std::monostate, void>> == 2);
+static_assert(std::variant_size_v<std::variant<const int, const float>> == 2);
+static_assert(std::variant_size_v<std::variant<std::variant<std::any>>> == 1);
+ 
+int main() { 
+  std::puts("All static assertions passed."); 
+}
