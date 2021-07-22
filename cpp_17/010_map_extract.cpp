0898d8eb19b8a334a667a7990dec891bcc0c383a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:56:01 +0800

    Update 010_map_extract.cpp

--- cpp_17/010_map_extract.cpp
+++ cpp_17/010_map_extract.cpp
@@ -1,1 +1,11 @@
-1
+// clang++ -std=c++17 -Wall -pedantic main.cpp
+#include<map>
+#include<string>
+#include<algorithm>
+
+int main() {
+  std::map<int, std::string> m{ {10, "potato"}, {1, "banana"} };
+  auto nodeHandler = m.extract(10);
+  nodeHandler.key() = 2;
+  m.insert(std::move(nodeHandler)); // { { 1, "banana" }, { 2, "potato" } }
+}
