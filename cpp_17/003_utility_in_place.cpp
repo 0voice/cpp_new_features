144af169e574cd6e93804d037a66201fafae1b43
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:37:48 +0800

    Update 003_utility_in_place.cpp

--- cpp_17/003_utility_in_place.cpp
+++ cpp_17/003_utility_in_place.cpp
@@ -1,1 +1,41 @@
-1
+#include <string>
+#include <functional>
+#include <iostream>
+#include <optional>
+ 
+// optional can be used as the return type of a factory that may fail
+std::optional<std::string> create(bool b) {
+    if (b)
+        return "Godzilla";
+    return {};
+}
+ 
+// std::nullopt can be used to create any (empty) std::optional
+auto create2(bool b) {
+    return b ? std::optional<std::string>{"Godzilla"} : std::nullopt;
+}
+ 
+// std::reference_wrapper may be used to return a reference
+auto create_ref(bool b) {
+    static std::string value = "Godzilla";
+    return b ? std::optional<std::reference_wrapper<std::string>>{value}
+             : std::nullopt;
+}
+ 
+int main()
+{
+    std::cout << "create(false) returned "
+              << create(false).value_or("empty") << '\n';
+ 
+    // optional-returning factory functions are usable as conditions of while and if
+    if (auto str = create2(true)) {
+        std::cout << "create2(true) returned " << *str << '\n';
+    }
+ 
+    if (auto str = create_ref(true)) {
+        // using get() to access the reference_wrapper's value
+        std::cout << "create_ref(true) returned " << str->get() << '\n';
+        str->get() = "Mothra";
+        std::cout << "modifying it changed it to " << str->get() << '\n';
+    }
+}
