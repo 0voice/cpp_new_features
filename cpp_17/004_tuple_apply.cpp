b24b2b7f8e1d925e6a155ee0b8a70095cb46aee5
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:49:17 +0800

    Update 004_tuple_apply.cpp

--- cpp_17/004_tuple_apply.cpp
+++ cpp_17/004_tuple_apply.cpp
@@ -1,1 +1,43 @@
-1
+#include <iostream>
+#include <tuple>
+#include <utility>
+ 
+int add(int first, int second) { return first + second; }
+ 
+template<typename T>
+T add_generic(T first, T second) { return first + second; }
+ 
+auto add_lambda = [](auto first, auto second) { return first + second; };
+ 
+template<typename... Ts>
+std::ostream& operator<<(std::ostream& os, std::tuple<Ts...> const& theTuple)
+{
+    std::apply
+    (
+        [&os](Ts const&... tupleArgs)
+        {
+            os << '[';
+            std::size_t n{0};
+            ((os << tupleArgs << (++n != sizeof...(Ts) ? ", " : "")), ...);
+            os << ']';
+        }, theTuple
+    );
+    return os;
+}
+ 
+int main()
+{
+    // OK
+    std::cout << std::apply(add, std::pair(1, 2)) << '\n';
+ 
+    // Error: can't deduce the function type
+    // std::cout << std::apply(add_generic, std::make_pair(2.0f, 3.0f)) << '\n'; 
+ 
+    // OK
+    std::cout << std::apply(add_lambda, std::pair(2.0f, 3.0f)) << '\n'; 
+ 
+    // advanced example
+    std::tuple myTuple(25, "Hello", 9.31f, 'c');
+    std::cout << myTuple << '\n';
+ 
+}
