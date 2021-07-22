2f7a5c05df9bdb53d2e9ad372453cafe1aae8509
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:58:24 +0800

    Update 011_unordered_map_extract.cpp

--- cpp_17/011_unordered_map_extract.cpp
+++ cpp_17/011_unordered_map_extract.cpp
@@ -1,1 +1,31 @@
-1
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+ 
+int main()
+{
+    std::unordered_map<int, char> cont{{1, 'a'}, {2, 'b'}, {3, 'c'}};
+ 
+    auto print = [](std::pair<const int, char>& n) { 
+        std::cout << " " << n.first << '(' << n.second << ')'; 
+    };
+ 
+    std::cout << "Start:";
+    std::for_each(cont.begin(), cont.end(), print);
+    std::cout << '\n';
+ 
+    // Extract node handle and change key
+    auto nh = cont.extract(1);
+    nh.key() = 4; 
+ 
+    std::cout << "After extract and before insert:";
+    std::for_each(cont.begin(), cont.end(), print);
+    std::cout << '\n';
+ 
+    // Insert node handle back
+    cont.insert(move(nh));
+ 
+    std::cout << "End:";
+    std::for_each(cont.begin(), cont.end(), print);
+    std::cout << '\n';
+}
