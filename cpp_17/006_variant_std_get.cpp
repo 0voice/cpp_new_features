3bd55b0d39b88748a57591a5e6ad97eaf63d49cd
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:23:57 +0800

    Update 006_variant_std_get.cpp

--- cpp_17/006_variant_std_get.cpp
+++ cpp_17/006_variant_std_get.cpp
@@ -1,1 +1,18 @@
-1
+#include <variant>
+#include <string>
+ 
+int main()
+{
+    std::variant<int, float> v{12}, w;
+    int i = std::get<int>(v);
+    w = std::get<int>(v);
+    w = std::get<0>(v); // same effect as the previous line
+ 
+//  std::get<double>(v); // error: no double in [int, float]
+//  std::get<3>(v);      // error: valid index values are 0 and 1
+ 
+    try {
+      std::get<float>(w); // w contains int, not float: will throw
+    }
+    catch (std::bad_variant_access&) {}
+}
