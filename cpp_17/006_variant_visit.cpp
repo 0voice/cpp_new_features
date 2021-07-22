52b4440f674677ea8c915e37ea0534036b8787f4
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:22:40 +0800

    Update 006_variant_visit.cpp

--- cpp_17/006_variant_visit.cpp
+++ cpp_17/006_variant_visit.cpp
@@ -1,1 +1,20 @@
-1
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <type_traits>
+#include <variant>
+#include <vector>
+
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+
+int main() {
+    std::vector<std::variant<int,long,double,std::string>> vec = { 10, 15l, 1.5, "hello" };
+    for (auto& v : vec) {
+        std::visit(overloaded{
+            [](auto arg) { std::cout << arg << ' '; },
+            [](double arg) { std::cout << std::fixed << arg << ' '; },
+            [](const std::string& arg) { std::cout << std::quoted(arg) << ' '; },
+            }, v);
+    }
+}
