e4ac91c9ac8f161b79c7dcd07a1050a2ed0075ca
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:20:37 +0800

    Update 005_optional_nullopt.cpp

--- cpp_17/005_optional_nullopt.cpp
+++ cpp_17/005_optional_nullopt.cpp
@@ -1,1 +1,10 @@
-1
+std::vector<int>> a;
+std::optional<std::vector<int>>& test(int b)
+{
+    a.clear();
+    a.push_back(b);
+    if(b)
+         return a;
+    else
+         return std::nullopt;
+}
