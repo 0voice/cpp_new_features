a670f4fa5257ddf68f14666e3c4b1ecae975918e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:52:25 +0800

    Update 009_initializer_list_empty.cpp

--- cpp_17/009_initializer_list_empty.cpp
+++ cpp_17/009_initializer_list_empty.cpp
@@ -1,1 +1,32 @@
-1
+#include <iostream>
+#include <vector>
+ 
+template <class T>
+void print(const T& container)
+{
+    if ( std::empty(container) )
+    {
+        std::cout << "Empty\n";
+    }
+    else
+    {
+        std::cout << "Elements:";
+        for ( const auto& element : container )
+            std::cout << ' ' << element;
+        std::cout << '\n';
+    }
+}
+ 
+int main() 
+{
+    std::vector<int> c = { 1, 2, 3 };
+    print(c);
+    c.clear();
+    print(c);
+ 
+    int array[] = { 4, 5, 6 };
+    print(array);
+ 
+    auto il = { 7, 8, 9 };
+    print(il);
+}
