1ddffe3e46d3d20b14915e12048912905a65d35b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 19:46:15 +0800

    Update 002_type_traits_byte.cpp

--- cpp_17/002_type_traits_byte.cpp
+++ cpp_17/002_type_traits_byte.cpp
@@ -1,1 +1,12 @@
-1
+#include <cstddef> // for std::byte
+ 
+std::byte b1{0x3F};
+std::byte b2{0b1111'0000};
+std::byte b4[4] {b1, b2, std::byte{1}}; // 4 bytes (last is 0)
+ 
+if (b1 == b4[0]) 
+{
+    b1 <<= 1;
+}
+ 
+std::cout << std::to_integer<int>(b1) << '\n'; // outputs: \T{126}
