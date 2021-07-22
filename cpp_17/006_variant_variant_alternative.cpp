5a31384c9742de46466e712f1d157bad24151873
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 21:30:12 +0800

    Update 006_variant_variant_alternative.cpp

--- cpp_17/006_variant_variant_alternative.cpp
+++ cpp_17/006_variant_variant_alternative.cpp
@@ -1,1 +1,16 @@
-1
+#include <variant>
+#include <iostream>
+ 
+using my_variant = std::variant<int, float>;
+static_assert(std::is_same_v
+    <int,   std::variant_alternative_t<0, my_variant>>);
+static_assert(std::is_same_v
+    <float, std::variant_alternative_t<1, my_variant>>);
+// cv-qualification on the variant type propagates to the extracted alternative type.
+static_assert(std::is_same_v
+    <const int, std::variant_alternative_t<0, const my_variant>>);
+ 
+int main()
+{
+    std::cout << "All static assertions passed.\n";
+}
