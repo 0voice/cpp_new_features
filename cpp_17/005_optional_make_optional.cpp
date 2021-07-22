b8e1e72ea7667acfb5c11c28765d9a87d5f4ff31
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:53:56 +0800

    Update 005_optional_make_optional.cpp

--- cpp_17/005_optional_make_optional.cpp
+++ cpp_17/005_optional_make_optional.cpp
@@ -1,1 +1,23 @@
-1
+#include <optional>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+ 
+int main()
+{
+    auto op1 = std::make_optional<std::vector<char>>({'a','b','c'});
+    std::cout << "op1: ";
+    for (char c: op1.value()){
+        std::cout << c << ",";
+    }
+    auto op2 = std::make_optional<std::vector<int>>(5, 2);
+    std::cout << "\nop2: ";
+    for (int i: *op2){
+        std::cout << i << ",";
+    }
+    std::string str{"hello world"};
+    auto op3 = std::make_optional<std::string>(std::move(str));
+    std::cout << "\nop3: " << quoted(op3.value_or("empty value")) << '\n';
+    std::cout << "str: " << std::quoted(str) << '\n';
+}
