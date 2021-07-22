a28147293d60e0b5c2964c4b2ce70a9b4e97c834
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-22 20:28:49 +0800

    Update 002_type_traits_conjunction.cpp

--- cpp_17/002_type_traits_conjunction.cpp
+++ cpp_17/002_type_traits_conjunction.cpp
@@ -1,1 +1,21 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+// 若所有 Ts... 都拥有等同于 T 的类型，则启用 func
+template<typename T, typename... Ts>
+std::enable_if_t<std::conjunction_v<std::is_same<T, Ts>...>>
+func(T, Ts...) {
+    std::cout << "all types in pack are T\n";
+}
+ 
+// 否则
+template<typename T, typename... Ts>
+std::enable_if_t<!std::conjunction_v<std::is_same<T, Ts>...>>
+func(T, Ts...) {
+    std::cout << "not all types in pack are T\n";
+}
+ 
+int main() {
+    func(1, 2, 3);
+    func(1, 2, "hello!");
+}
