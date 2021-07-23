929a6b5d24dbd6897c884a3156692fac7ea6c57e
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:44:40 +0800

    Update 010_concepts_equality_comparable_with.cpp

--- cpp_20/010_concepts_equality_comparable_with.cpp
+++ cpp_20/010_concepts_equality_comparable_with.cpp
@@ -1,1 +1,16 @@
-1
+class strong_int {
+public:
+    using integral_type = int;
+
+    explicit strong_int( integral_type ) noexcept;
+
+    explicit operator integral_type() const noexcept;
+
+    constexpr auto operator<=>( const strong_int& ) const noexcept;
+
+private:
+    integral_type m_value;
+};
+
+constexpr auto operator<=>( const strong_int&, const int& ) noexcept;
+constexpr auto operator<=>( const int&, const strong_int& ) noexcept;
