ee47cfa31fd02c0cee4892dc737dbc3c831bd452
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:46:26 +0800

    Update 010_concepts_semiregular.cpp

--- cpp_20/010_concepts_semiregular.cpp
+++ cpp_20/010_concepts_semiregular.cpp
@@ -1,1 +1,16 @@
-1
+#include <concepts>
+#include <iostream>
+ 
+template<std::semiregular T>
+struct Single {
+    T value;
+};
+ 
+int main()
+{
+    Single<int> myInt1{4};
+    Single<int> myInt2;
+    myInt2 = myInt1;
+ 
+    std::cout << myInt1.value << ' ' << myInt2.value << '\n';
+}
