9003fade16d77961e85f7a59a3c2c601f396cc71
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:56:44 +0800

    Update 011_memory_ranges_uninitialized_copy.cpp

--- cpp_20/011_memory_ranges_uninitialized_copy.cpp
+++ cpp_20/011_memory_ranges_uninitialized_copy.cpp
@@ -1,1 +1,33 @@
-1
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+ 
+int main()
+{
+    const char* v[] { "This", "is", "an", "example", };
+ 
+    if (const auto sz{std::size(v)};
+        void* pbuf = std::aligned_alloc(alignof(std::string), sizeof(std::string) * sz))
+    {
+        try
+        {
+            auto first {static_cast<std::string*>(pbuf)};
+            auto last {first + sz};
+            std::ranges::uninitialized_copy(std::begin(v), std::end(v), first, last);
+ 
+            std::cout << "{ ";
+            for (auto it {first}; it != last; ++it)
+                std::cout << std::quoted(*it) << ", ";
+            std::cout << "};\n";
+ 
+            std::ranges::destroy(first, last);
+        }
+        catch(...)
+        {
+            std::cout << "uninitialized_copy exception\n";
+        }
+        std::free(pbuf);
+    }
+}
