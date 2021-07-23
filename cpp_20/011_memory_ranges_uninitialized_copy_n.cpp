adcb68d1e94a88aab0fd99bf64d00a0ca6db2723
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:57:14 +0800

    Update 011_memory_ranges_uninitialized_copy_n.cpp

--- cpp_20/011_memory_ranges_uninitialized_copy_n.cpp
+++ cpp_20/011_memory_ranges_uninitialized_copy_n.cpp
@@ -1,1 +1,30 @@
-1
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+ 
+int main()
+{
+    const char* stars[] { "Procyon", "Spica", "Pollux", "Deneb", "Polaris", };
+ 
+    constexpr int n {4};
+    alignas(alignof(std::string)) char out[n * sizeof(std::string)];
+ 
+    try
+    {
+        auto first {reinterpret_cast<std::string*>(out)};
+        auto last {first + n};
+        auto ret {std::ranges::uninitialized_copy_n(std::begin(stars), n, first, last)};
+ 
+        std::cout << "{ ";
+        for (auto it {first}; it != ret.out; ++it)
+            std::cout << std::quoted(*it) << ", ";
+        std::cout << "};\n";
+ 
+        std::ranges::destroy(first, last);
+    }
+    catch(...)
+    {
+        std::cout << "uninitialized_copy_n exception\n";
+    }
+}
