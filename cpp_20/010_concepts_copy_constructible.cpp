c848af501c12df082879888694c52727997e998c
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:42:30 +0800

    Update 010_concepts_copy_constructible.cpp

--- cpp_20/010_concepts_copy_constructible.cpp
+++ cpp_20/010_concepts_copy_constructible.cpp
@@ -1,1 +1,21 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+struct Ex1 {
+    std::string str; // member has a non-trivial copy ctor
+};
+struct Ex2 {
+    int n;
+    Ex2(const Ex2&) = default; // trivial and non-throwing
+};
+ 
+int main() {
+    std::cout << std::boolalpha << "Ex1 is copy-constructible? "
+              << std::is_copy_constructible<Ex1>::value << '\n'
+              << "Ex1 is trivially copy-constructible? "
+              << std::is_trivially_copy_constructible<Ex1>::value << '\n'
+              << "Ex2 is trivially copy-constructible? "
+              << std::is_trivially_copy_constructible<Ex2>::value << '\n'
+              << "Ex2 is nothrow copy-constructible? "
+              << std::is_nothrow_copy_constructible<Ex2>::value << '\n';
+}
