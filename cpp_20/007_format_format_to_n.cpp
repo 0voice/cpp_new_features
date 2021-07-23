47731b23c284971cd4eeeb18b407ebbfe0376b96
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 20:38:50 +0800

    Update 007_format_format_to_n.cpp

--- cpp_20/007_format_format_to_n.cpp
+++ cpp_20/007_format_format_to_n.cpp
@@ -1,1 +1,17 @@
-1
+#include <format>
+#include <string_view>
+#include <iostream>
+ 
+int main()
+{
+    char buffer[64];
+ 
+    const auto result =
+        std::format_to_n(buffer, std::size(buffer), 
+                         "Hubble's H{0} {1} {2} km/sec/mpc.",
+                         "\u2080", "\u2245", 71);
+ 
+    std::cout << "Buffer: \"" << std::string_view{buffer, result.size} << "\"\n"
+              << "Buffer size = " << std::size(buffer) << '\n'
+              << "Untruncated output size = " << result.size << '\n';
+}
