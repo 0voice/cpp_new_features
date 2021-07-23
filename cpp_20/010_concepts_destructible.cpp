6ae48da9096d39856a1836b157c9b6f58a55f0aa
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:40:00 +0800

    Update 010_concepts_destructible.cpp

--- cpp_20/010_concepts_destructible.cpp
+++ cpp_20/010_concepts_destructible.cpp
@@ -1,1 +1,21 @@
-1
+#include <iostream>
+#include <string>
+#include <type_traits>
+struct Foo {
+   std::string str;
+   ~Foo() noexcept {};
+};
+struct Bar {
+    ~Bar() = default;
+};
+int main() {
+    std::cout << std::boolalpha
+              << "std::string is destructible? "
+              << std::is_destructible<std::string>::value << '\n'
+              << "Foo is trivially destructible? "
+              << std::is_trivially_destructible_v<Foo> << '\n'
+              << "Foo is nothrow destructible? "
+              << std::is_nothrow_destructible<Foo>() << '\n'
+              << "Bar is trivially destructible? "
+              << std::is_trivially_destructible<Bar>{} << '\n';
+}
