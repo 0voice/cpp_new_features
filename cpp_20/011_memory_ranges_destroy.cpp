6cdc1af417d7237a18622ced6774a731cb34e776
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 22:02:07 +0800

    Update 011_memory_ranges_destroy.cpp

--- cpp_20/011_memory_ranges_destroy.cpp
+++ cpp_20/011_memory_ranges_destroy.cpp
@@ -1,1 +1,20 @@
-1
+#include <memory>
+#include <new>
+#include <iostream>
+ 
+struct Tracer {
+    int value;
+    ~Tracer() { std::cout << value << " destructed\n"; }
+};
+ 
+int main()
+{
+    alignas(Tracer) unsigned char buffer[sizeof(Tracer) * 8];
+ 
+    for (int i = 0; i < 8; ++i)
+        new(buffer + sizeof(Tracer) * i) Tracer{i}; //manually construct objects
+ 
+    auto ptr = std::launder(reinterpret_cast<Tracer*>(buffer));
+ 
+    std::ranges::destroy(ptr, ptr + 8);
+}
