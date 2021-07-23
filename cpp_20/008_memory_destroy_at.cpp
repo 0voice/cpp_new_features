73736617ea7d310dc2a983df8b3aad3e83072873
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:15:23 +0800

    Update 008_memory_destroy_at.cpp

--- cpp_20/008_memory_destroy_at.cpp
+++ cpp_20/008_memory_destroy_at.cpp
@@ -1,1 +1,20 @@
-1
+#include <memory>
+#include <new>
+#include <iostream>
+ 
+struct Tracer {
+    int value;
+    ~Tracer() { std::cout << value << " destructed\n"; }
+};
+ 
+int main()
+{
+    alignas(Tracer) unsigned char buffer[sizeof(Tracer) * 8];
+ 
+    for (int i = 0; i < 8; ++i)
+        new(buffer + sizeof(Tracer) * i) Tracer{i}; // 手工构造对象
+ 
+    auto ptr = std::launder(reinterpret_cast<Tracer*>(buffer));
+ 
+    std::destroy(ptr, ptr + 8);
+}
