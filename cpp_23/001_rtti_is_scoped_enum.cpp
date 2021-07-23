f740fa0efd71c670a533a6355f4c6e673b8887d4
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 19:43:36 +0800

    Update 001_rtti_is_scoped_enum.cpp

--- cpp_23/001_rtti_is_scoped_enum.cpp
+++ cpp_23/001_rtti_is_scoped_enum.cpp
@@ -1,1 +1,20 @@
-1
+#include <iostream>
+#include <type_traits>
+ 
+class A {};
+ 
+enum E {};
+ 
+enum struct Es { oz };
+ 
+enum class Ec : int {};
+ 
+int main() 
+{
+    std::cout << std::boolalpha;
+    std::cout << std::is_scoped_enum_v<A> << '\n';
+    std::cout << std::is_scoped_enum_v<E> << '\n';
+    std::cout << std::is_scoped_enum_v<Es> << '\n';
+    std::cout << std::is_scoped_enum_v<Ec> << '\n';
+    std::cout << std::is_scoped_enum_v<int> << '\n';
+}
