4f1ff986cdf5bde06e4a5b46b09c002892abc06b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:31:53 +0800

    Update 010_concepts_convertible_to.cpp

--- cpp_20/010_concepts_convertible_to.cpp
+++ cpp_20/010_concepts_convertible_to.cpp
@@ -1,1 +1,10 @@
-1
+struct From;
+struct To {
+    explicit To(From) = delete;
+};
+struct From {
+    operator To();
+};
+
+static_assert(std::is_convertible_v<From, To>);
+static_assert(not std::convertible_to<From, To>);
