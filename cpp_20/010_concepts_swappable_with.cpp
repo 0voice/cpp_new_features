4c7e43adcf95b4941a72076232beeb6dd5c8cc5a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:39:33 +0800

    Update 010_concepts_swappable_with.cpp

--- cpp_20/010_concepts_swappable_with.cpp
+++ cpp_20/010_concepts_swappable_with.cpp
@@ -1,1 +1,16 @@
-1
+#include <concepts> 
+
+template <typename T, typename U>
+requires std::swappable_with<T,U>
+void mySwap(T& t, U& u)
+{
+    T temp = t; t = u; u = temp;
+}
+
+int main()
+{
+    int x, y;
+    mySwap(x, y);
+
+    return 0;
+}
