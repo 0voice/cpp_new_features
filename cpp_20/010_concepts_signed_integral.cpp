9d77e9366de3414c0e0d8d601edbc22cf867b812
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:33:58 +0800

    Update 010_concepts_signed_integral.cpp

--- cpp_20/010_concepts_signed_integral.cpp
+++ cpp_20/010_concepts_signed_integral.cpp
@@ -1,1 +1,23 @@
-1
+#include <concepts>
+#include <iostream>
+ 
+void print(std::signed_integral auto i) {
+    std::cout << "Signed integral: " << i << '\n';
+}
+ 
+void print(std::unsigned_integral auto u) {
+    std::cout << "Unsigned integral: " << u << '\n';
+}
+ 
+void print(auto x) {
+    std::cout << "Non-integral: " << x << '\n';
+}
+ 
+int main() {
+    print(42);     // signed
+    print(0xFull); // unsigned
+    print(true);   // unsigned
+    print('A');    // platform-dependent
+    print(4e-2);   // non-integral (hex-float)
+    print("∫∫∫");  // non-integral
+}
