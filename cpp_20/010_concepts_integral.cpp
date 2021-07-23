aeccb7a124ee7abce9928033df8918a11f8e32fe
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:33:32 +0800

    Update 010_concepts_integral.cpp

--- cpp_20/010_concepts_integral.cpp
+++ cpp_20/010_concepts_integral.cpp
@@ -1,1 +1,18 @@
-1
+#include <concepts>
+#include <iostream>
+ 
+void print(std::integral auto i) {
+    std::cout << "Integral: " << i << '\n';
+}
+ 
+void print(auto x) {
+    std::cout << "Non-integral: " << x << '\n';
+}
+ 
+int main()
+{
+    print( 'o' ); static_assert( std::integral<char> );
+    print( 007 ); static_assert( std::integral<int> );
+    print( 2e2 ); static_assert( !std::integral<float> );
+    print("∫∫∫"); static_assert( !std::integral<decltype("")> );
+}
