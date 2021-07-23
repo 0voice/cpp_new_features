855888d166c1095f6a3bb5e507825ca726f22bb9
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:31:13 +0800

    Update 010_concepts_derived_from.cpp

--- cpp_20/010_concepts_derived_from.cpp
+++ cpp_20/010_concepts_derived_from.cpp
@@ -1,1 +1,21 @@
-1
+#include <concepts>
+ 
+class A {};
+ 
+class B: public A {};
+ 
+class C: private A{};
+ 
+int main() {
+    // std::derived_from == true only for public inheritance or exact same class
+    static_assert( std::derived_from<B, B> == true );      // same class: true
+    static_assert( std::derived_from<int, int> == false ); // same primitive type: false
+    static_assert( std::derived_from<B, A> == true );      // public inheritance: true
+    static_assert( std::derived_from<C, A> == false );     // private inheritance: false
+ 
+    // std::is_base_of == true also for private inheritance
+    static_assert( std::is_base_of_v<B, B> == true );      // same class: true
+    static_assert( std::is_base_of_v<int, int> == false ); // same primitive type: false
+    static_assert( std::is_base_of_v<A, B> == true );      // public inheritance: true
+    static_assert( std::is_base_of_v<A, C> == true );      // private inheritance: true
+}
