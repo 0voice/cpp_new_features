a2245381cea3bd2322b572368593450ecb949f03
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:58:15 +0800

    Update 011_memory_ranges_uninitialized_fill.cpp

--- cpp_20/011_memory_ranges_uninitialized_fill.cpp
+++ cpp_20/011_memory_ranges_uninitialized_fill.cpp
@@ -1,1 +1,27 @@
-1
+#include <iostream>
+#include <memory>
+#include <string>
+ 
+int main()
+{
+    constexpr int n {4};
+    alignas(alignof(std::string)) char out[n * sizeof(std::string)];
+ 
+    try
+    {
+        auto first {reinterpret_cast<std::string*>(out)};
+        auto last {first + n};
+        std::ranges::uninitialized_fill(first, last, "▄▀▄▀▄▀▄▀");
+ 
+        int count {1};
+        for (auto it {first}; it != last; ++it) {
+            std::cout << count++ << ' ' << *it << '\n';
+        }
+ 
+        std::ranges::destroy(first, last);
+    }
+    catch(...)
+    {
+        std::cout << "Exception!\n";
+    }
+}
