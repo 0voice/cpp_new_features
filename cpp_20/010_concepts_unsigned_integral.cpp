9c798bf7970fbe89e6f07d4944c9507bcdcb05e9
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:34:40 +0800

    Update 010_concepts_unsigned_integral.cpp

--- cpp_20/010_concepts_unsigned_integral.cpp
+++ cpp_20/010_concepts_unsigned_integral.cpp
@@ -1,1 +1,17 @@
-1
+#include <iostream>
+#include <concepts>
+template<typename T>
+concept gcdint = std::unsigned_integral<T> && !std::is_same_v<std::remove_cv_t<T>, bool>;
+
+template<gcdint T1, gcdint T2>
+constexpr std::common_type_t<T1, T2> gcd(T1 a, T2 b)
+{
+	return b ? gcd(b, a % b) : a;
+}
+int main()
+{
+	std::cout << gcd(37u, 666u) << std::endl;
+	std::cout << gcd(0u, false) << std::endl;
+	return 0;
+}
+
