7ad0033f843e1966c2ad708d08935304c3f5b941
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 19:44:05 +0800

    Update 002_utility_to_underlying.cpp

--- cpp_23/002_utility_to_underlying.cpp
+++ cpp_23/002_utility_to_underlying.cpp
@@ -1,1 +1,28 @@
-1
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+#include <utility>
+ 
+int main()
+{
+    enum class E1 : char { e };
+    static_assert(std::is_same_v<char, decltype(std::to_underlying(E1::e))>);
+    enum struct E2 : long { e };
+    static_assert(std::is_same_v<long, decltype(std::to_underlying(E2::e))>);
+    enum E3 : unsigned { e };
+    static_assert(std::is_same_v<unsigned, decltype(std::to_underlying(e))>);
+ 
+    enum class ColorMask : std::uint32_t {
+        red = 0xFF, green = (red << 8), blue = (green << 8), alpha = (blue << 8)
+    };
+    std::cout << std::hex << std::uppercase << std::setfill('0')
+        << std::setw(8) << std::to_underlying(ColorMask::red) << '\n'
+        << std::setw(8) << std::to_underlying(ColorMask::green) << '\n'
+        << std::setw(8) << std::to_underlying(ColorMask::blue) << '\n'
+        << std::setw(8) << std::to_underlying(ColorMask::alpha) << '\n';
+ 
+//  std::underlying_type_t<ColorMask> x = ColorMask::alpha; // Error: no known conversion
+    [[maybe_unused]]
+    std::underlying_type_t<ColorMask> y = std::to_underlying(ColorMask::alpha); // OK
+}
