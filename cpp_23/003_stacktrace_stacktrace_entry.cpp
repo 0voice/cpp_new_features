ea4039e97796e8833beca1a44ed605c41be644ae
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 19:51:10 +0800

    Update 003_stacktrace_stacktrace_entry.cpp

--- cpp_23/003_stacktrace_stacktrace_entry.cpp
+++ cpp_23/003_stacktrace_stacktrace_entry.cpp
@@ -1,1 +1,52 @@
-1
+#include <iostream>
+#include <string_view>
+#include <source_location>
+ 
+void log(const std::string_view message,
+         const std::source_location location = 
+               std::source_location::current())
+{
+    std::cout << "file: "
+              << location.file_name() << "("
+              << location.line() << ":"
+              << location.column() << ") `"
+              << location.function_name() << "`: "
+              << message << '\n';
+}
+ 
+template <typename T> void fun(T x)
+{
+    log(x);
+}
+ 
+int main(int, char*[])
+{
+    log("Hello world!");
+    fun("Hello C++20!");
+}
+#include <iostream>
+#include <string_view>
+#include <source_location>
+ 
+void log(const std::string_view message,
+         const std::source_location location = 
+               std::source_location::current())
+{
+    std::cout << "file: "
+              << location.file_name() << "("
+              << location.line() << ":"
+              << location.column() << ") `"
+              << location.function_name() << "`: "
+              << message << '\n';
+}
+ 
+template <typename T> void fun(T x)
+{
+    log(x);
+}
+ 
+int main(int, char*[])
+{
+    log("Hello world!");
+    fun("Hello C++20!");
+}
