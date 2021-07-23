463c646d234fa4dcdfa94cbf0b40e4217d6a7903
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:59:15 +0800

    Update 011_memory_ranges_uninitialized_move.cpp

--- cpp_20/011_memory_ranges_uninitialized_move.cpp
+++ cpp_20/011_memory_ranges_uninitialized_move.cpp
@@ -1,1 +1,36 @@
-1
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+ 
+void print(auto rem, auto first, auto last) {
+    for (std::cout << rem; first != last; ++first)
+        std::cout << std::quoted(*first) << ' ';
+    std::cout << '\n';
+}
+ 
+int main() {
+    std::string in[] { "Home", "World" };
+    print("initially, in: ", std::begin(in), std::end(in));
+ 
+    if (
+        constexpr auto sz = std::size(in);
+        void* out = std::aligned_alloc(alignof(std::string), sizeof(std::string) * sz)
+    ) {
+        try {
+            auto first {static_cast<std::string*>(out)};
+            auto last {first + sz};
+            std::ranges::uninitialized_move(std::begin(in), std::end(in), first, last);
+ 
+            print("after move, in: ", std::begin(in), std::end(in));
+            print("after move, out: ", first, last);
+ 
+            std::ranges::destroy(first, last);
+        }
+        catch (...) {
+            std::cout << "Exception!\n";
+        }
+        std::free(out);
+    }
+}
