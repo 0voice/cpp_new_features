368226dd9fb48ac0da141b8aaea8e410f167f2a4
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 22:01:43 +0800

    Update 011_memory_ranges_destroy_at.cpp

--- cpp_20/011_memory_ranges_destroy_at.cpp
+++ cpp_20/011_memory_ranges_destroy_at.cpp
@@ -1,1 +1,26 @@
-1
+#include <iostream>
+#include <memory>
+ 
+struct S {
+    int x;
+    float y;
+    double z;
+ 
+    S(int x, float y, double z) : x{x}, y{y}, z{z} { std::cout << "S::S();\n"; }
+ 
+    ~S() { std::cout << "S::~S();\n"; }
+ 
+    void print() const {
+        std::cout << "S { x=" << x << "; y=" << y << "; z=" << z << "; };\n";
+    }
+};
+ 
+int main()
+{
+    alignas(S) unsigned char buf[sizeof(S)];
+ 
+    S* ptr = std::ranges::construct_at(reinterpret_cast<S*>(buf), 42, 2.71828f, 3.1415);
+    ptr->print();
+ 
+    std::ranges::destroy_at(ptr);
+}
