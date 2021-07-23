571119cc8a60f027ee1cb111714c6c45bf66f765
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 20:28:40 +0800

    Update 007_format_format_to.cpp

--- cpp_20/007_format_format_to.cpp
+++ cpp_20/007_format_format_to.cpp
@@ -1,1 +1,36 @@
-1
+#include <format>
+#include <iostream>
+#include <iterator>
+#include <string>
+ 
+auto main() -> int
+{
+    std::string buffer;
+ 
+    std::format_to(
+        std::back_inserter(buffer), //< OutputIt
+        "Hello, C++{}!\n",          //< fmt 
+        "20");                      //< arg
+    std::cout << buffer;
+    buffer.clear();
+ 
+    std::format_to(
+        std::back_inserter(buffer), //< OutputIt
+        "Hello, {0}::{1}!{2}",      //< fmt 
+        "std",                      //< arg {0}
+        "format_to()",              //< arg {1}
+        "\n",                       //< arg {2}
+        "extra param(s)...");       //< 不使用
+    std::cout << buffer;
+ 
+    std::wstring wbuffer;
+    std::format_to(
+        std::back_inserter(wbuffer),//< OutputIt 
+        L"Hello, {2}::{1}!{0}",     //< fmt
+        L"\n",                      //< arg {0}
+        L"format_to()",             //< arg {1}
+        L"std",                     //< arg {2}
+        L"...is not..."             //< 不使用
+        L"...an error!");           //< 不使用
+    std::wcout << wbuffer;
+}
