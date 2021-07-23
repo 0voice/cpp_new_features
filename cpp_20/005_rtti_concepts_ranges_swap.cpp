f7d78e9ddd4932f8c25aa7b15054ac35015af468
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 20:20:00 +0800

    Update 005_rtti_concepts_ranges_swap.cpp

--- cpp_20/005_rtti_concepts_ranges_swap.cpp
+++ cpp_20/005_rtti_concepts_ranges_swap.cpp
@@ -1,1 +1,26 @@
-1
+
+#include<iostream>
+#include<time.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+ 
+int main()
+{
+	vector<int> v;
+ 
+	srand(time(0));
+	for(int i=0;i<10;i++)
+	{
+		v.push_back(i+1);
+	}
+	
+	//使用了lambda表达式
+	for_each(v.begin(),v.end(),[](int n){cout<<n<<' ';});
+	cout<<endl;
+ 
+	swap_ranges(v.begin(),v.begin()+5,v.begin()+5);
+ 
+	for_each(v.begin(),v.end(),[](int n){cout<<n<<' ';});
+	cout<<endl;
+}
