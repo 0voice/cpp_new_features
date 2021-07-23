2e9af9df8e5143bca6b4e03684ddc1a7d6755cc6
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:30:46 +0800

    Update 010_concepts_same_as.cpp

--- cpp_20/010_concepts_same_as.cpp
+++ cpp_20/010_concepts_same_as.cpp
@@ -1,1 +1,16 @@
-1
+#include <concepts>
+#include <iostream>
+ 
+template<typename T, typename ... U>
+concept IsAnyOf = (std::same_as<T, U> || ...);
+ 
+template<typename T>
+concept IsPrintable = std::integral<T> || std::floating_point<T> ||
+    IsAnyOf<std::remove_cvref_t<std::remove_pointer_t<std::decay_t<T>>>, char, wchar_t>;
+ 
+void println(IsPrintable auto const ... arguments)
+{
+    (std::wcout << ... << arguments) << '\n';
+}
+ 
+int main() { println("Example: ", 3.14, " : ", 42, " : [", 'a', L'-', L"Z]"); }
