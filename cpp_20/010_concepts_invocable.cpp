f7cc4538e7a1b6a7d075e3b7d6895cb687f89e57
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:47:05 +0800

    Update 010_concepts_invocable.cpp

--- cpp_20/010_concepts_invocable.cpp
+++ cpp_20/010_concepts_invocable.cpp
@@ -1,1 +1,18 @@
-1
+#include <type_traits>
+ 
+auto func2(char) -> int (*)()
+{
+    return nullptr;
+}
+ 
+int main()
+{
+    static_assert( std::is_invocable_v<int()> );
+    static_assert( not std::is_invocable_v<int(), int> );
+    static_assert( std::is_invocable_r_v<int, int()> );
+    static_assert( not std::is_invocable_r_v<int*, int()> );
+    static_assert( std::is_invocable_r_v<void, void(int), int> );
+    static_assert( not std::is_invocable_r_v<void, void(int), void> );
+    static_assert( std::is_invocable_r_v<int(*)(), decltype(func2), char> );
+    static_assert( not std::is_invocable_r_v<int(*)(), decltype(func2), void> );
+}
