ec4071d40272afdc107ef3af5881ad1a74cbd693
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 22:00:03 +0800

    Update 011_memory_ranges_uninitialized_default_construct.cpp

--- cpp_20/011_memory_ranges_uninitialized_default_construct.cpp
+++ cpp_20/011_memory_ranges_uninitialized_default_construct.cpp
@@ -1,1 +1,42 @@
-1
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <string>
+ 
+int main()
+{
+    struct S { std::string m{ "▄▀▄▀▄▀▄▀" }; };
+ 
+    constexpr int n {4};
+    alignas(alignof(S)) char out[n * sizeof(S)];
+ 
+    try
+    {
+        auto first {reinterpret_cast<S*>(out)};
+        auto last {first + n};
+ 
+        std::ranges::uninitialized_default_construct(first, last);
+ 
+        auto count {1};
+        for (auto it {first}; it != last; ++it) {
+            std::cout << count++ << ' ' << it->m << '\n';
+        }
+ 
+        std::ranges::destroy(first, last);
+    }
+    catch(...) { std::cout << "Exception!\n"; }
+ 
+    // Notice that for "trivial types" the uninitialized_default_construct
+    // generally does not zero-fill the given uninitialized memory area.
+    constexpr char etalon[] { 'A', 'B', 'C', 'D', '\n' };
+    char v[] { 'A', 'B', 'C', 'D', '\n' };
+    std::ranges::uninitialized_default_construct(std::begin(v), std::end(v));
+    if (std::memcmp(v, etalon, sizeof(v)) == 0) {
+        std::cout << "  ";
+        // Maybe undefined behavior, pending CWG 1997:
+        // for (const char c : v) { std::cout << c << ' '; }
+        for (const char c : etalon) { std::cout << c << ' '; }
+    } else {
+        std::cout << "Unspecified\n";
+    }
+}
