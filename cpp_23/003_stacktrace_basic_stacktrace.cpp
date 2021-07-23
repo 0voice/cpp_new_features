16f745a421e3a5251f2d33014a92b2bc1a02e1dd
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 19:53:14 +0800

    Update 003_stacktrace_basic_stacktrace.cpp

--- cpp_23/003_stacktrace_basic_stacktrace.cpp
+++ cpp_23/003_stacktrace_basic_stacktrace.cpp
@@ -1,1 +1,12 @@
-1
+#include <stacktrace>
+#include <iostream>
+ 
+int main()
+{
+    std::cout << std::boolalpha;
+    std::stacktrace bktr;
+    std::cout << "Initially, bktr.empty(): " << bktr.empty() << '\n';
+ 
+    bktr = std::stacktrace::current();
+    std::cout << "After getting entries, bktr.empty(): " << bktr.empty() << '\n';
+}
