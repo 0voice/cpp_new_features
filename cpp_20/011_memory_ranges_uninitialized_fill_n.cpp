ea6aa815cf0f1451e4edcf0bf4102e63469e2ae0
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:58:49 +0800

    Update 011_memory_ranges_uninitialized_fill_n.cpp

--- cpp_20/011_memory_ranges_uninitialized_fill_n.cpp
+++ cpp_20/011_memory_ranges_uninitialized_fill_n.cpp
@@ -1,1 +1,25 @@
-1
+#include <iostream>
+#include <memory>
+#include <string>
+ 
+int main()
+{
+    constexpr int n {3};
+    alignas(alignof(std::string)) char out[n * sizeof(std::string)];
+ 
+    try
+    {
+        auto first {reinterpret_cast<std::string*>(out)};
+        auto last = std::ranges::uninitialized_fill_n(first, n, "cppreference");
+ 
+        for (auto it {first}; it != last; ++it) {
+            std::cout << *it << '\n';
+        }
+ 
+        std::ranges::destroy(first, last);
+    }
+    catch(...)
+    {
+        std::cout << "Exception!\n";
+    }
+}
