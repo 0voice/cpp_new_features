18efd75c859a6f5a027ecf7a04ac9f31d12fd77f
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-23 21:37:02 +0800

    Update 010_concepts_assignable_from.cpp

--- cpp_20/010_concepts_assignable_from.cpp
+++ cpp_20/010_concepts_assignable_from.cpp
@@ -1,1 +1,19 @@
-1
+#include <iostream>
+
+template <typename T>
+class A
+{
+public:
+    const A& operator= (const A& other)    {  return *this;    }
+};
+
+int main ()
+{
+    A<double> d1, d2; d1 = d2; //this works fine
+
+    std::cout << std::boolalpha
+        << "Is double assignable?    " << std::assignable_from<double&, double> << '\n' //Says true, as it should
+        << "Is A<double> assignable? " << std::assignable_from<A<double>&, A<double>> << '\n'; //Says false
+
+    return 0;
+}
