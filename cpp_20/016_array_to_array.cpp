e687a78bd06277d3cc7677b7a63801367b7c3469
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:26:47 +0800

    Update 016_array_to_array.cpp

--- cpp_20/016_array_to_array.cpp
+++ cpp_20/016_array_to_array.cpp
@@ -1,1 +1,8 @@
-1
+template<class T, size_t N, class V>
+std::array<T, N> to_array(const V& v)
+{
+    assert(v.size() == N);
+    std::array<T, N> d;
+    std::copy(v.begin(), v.end(), d.data());
+    return d;
+}
