663fff29b6629d687051b3c0cf747f72b0a1a727
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:29:14 +0800

    Update 018_map_contains.cpp

--- cpp_20/018_map_contains.cpp
+++ cpp_20/018_map_contains.cpp
@@ -1,1 +1,13 @@
-1
+#include <iostream>
+#include <set>
+ 
+int main()
+{  
+    std::set<int> example = {1, 2, 3, 4};
+ 
+    if (example.contains(2)) {
+        std::cout << "Found\n";
+    } else {
+        std::cout << "Not found\n";
+    }
+}
