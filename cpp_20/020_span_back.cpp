03feac082bc859a7ec6462fc0cd844c3128ea105
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:30:48 +0800

    Update 020_span_back.cpp

--- cpp_20/020_span_back.cpp
+++ cpp_20/020_span_back.cpp
@@ -1,1 +1,19 @@
-1
+#include <iostream>     // std::cout
+#include <iterator>     // std::back_inserter
+#include <vector>       // std::vector
+#include <algorithm>    // std::copy
+
+int main () {
+  std::vector<int> foo,bar;
+  for (int i=1; i<=5; i++)
+  { foo.push_back(i); bar.push_back(i*10); }
+
+  std::copy (bar.begin(),bar.end(),back_inserter(foo));
+
+  std::cout << "foo contains:";
+  for ( std::vector<int>::iterator it = foo.begin(); it!= foo.end(); ++it )
+	  std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  return 0;
+}
