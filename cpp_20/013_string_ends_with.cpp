e1a8eec22c1e4a7d1f5eff1070771ecc55ef9293
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:24:41 +0800

    Update 013_string_ends_with.cpp

--- cpp_20/013_string_ends_with.cpp
+++ cpp_20/013_string_ends_with.cpp
@@ -1,1 +1,24 @@
-1
+#include<string>
+#include<iostream>
+using namespace std;
+
+int startsWith(string s, string sub){
+        return s.find(sub)==0?1:0;
+}
+
+int endsWith(string s,string sub){
+        return s.rfind(sub)==(s.length()-sub.length())?1:0;
+}
+
+int main(){
+        string str = "helloWorld";
+        string preStr = "he";
+        string sufStr = "rld";
+        if(startsWith(str,preStr)){
+                cout<<str + " starts with "+preStr<<endl;
+        }
+        if(endsWith(str,sufStr)){
+                cout<<str+" ends with "+sufStr<<endl;
+        }
+        return 0;
+}
