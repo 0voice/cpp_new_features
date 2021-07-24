eda65831d4f6effeef645bd110142f61a6c1f756
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:33:06 +0800

    Update 020_span_rbegin.cpp

--- cpp_20/020_span_rbegin.cpp
+++ cpp_20/020_span_rbegin.cpp
@@ -1,1 +1,14 @@
-1
+#include <iostream>
+#include <vector>
+#include <iterator>
+ 
+int main() 
+{
+    std::vector<int> v = { 3, 1, 4 };
+    auto vi = std::rbegin(v);
+    std::cout << *vi << '\n'; 
+ 
+    int a[] = { -5, 10, 15 };
+    auto ai = std::rbegin(a);
+    std::cout << *ai << '\n';
+}
