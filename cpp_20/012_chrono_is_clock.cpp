37834189a1850269c4b1068b45db6ee2b1211144
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:15:40 +0800

    Update 012_chrono_is_clock.cpp

--- cpp_20/012_chrono_is_clock.cpp
+++ cpp_20/012_chrono_is_clock.cpp
@@ -1,1 +1,15 @@
-1
+
+template<class>
+struct is_clock : std::false_type {};
+ 
+template<class T>
+    requires
+        requires {
+            typename T::rep;
+            typename T::period;
+            typename T::duration;
+            typename T::time_point;
+            T::is_steady;
+            T::now();
+        }
+struct is_clock<T> : std::true_type {};
