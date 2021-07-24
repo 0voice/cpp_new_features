120065491ff82ff7294f9ed63016631adc9c949a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:32:38 +0800

    Update 020_span_rend.cpp

--- cpp_20/020_span_rend.cpp
+++ cpp_20/020_span_rend.cpp
@@ -1,1 +1,15 @@
-1
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+ 
+int main()
+{
+    int a[] = {4, 6, -3, 9, 10};
+    std::cout << "Array backwards: ";
+    std::copy(std::rbegin(a), std::rend(a), std::ostream_iterator<int>(std::cout, " "));
+ 
+    std::cout << "\nVector backwards: ";
+    std::vector<int> v = {4, 6, -3, 9, 10};
+    std::copy(std::rbegin(v), std::rend(v), std::ostream_iterator<int>(std::cout, " "));
+}
