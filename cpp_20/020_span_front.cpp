2a6f67e00861b08413003bedcf08030e4321fb37
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:32:08 +0800

    Update 020_span_front.cpp

--- cpp_20/020_span_front.cpp
+++ cpp_20/020_span_front.cpp
@@ -1,1 +1,15 @@
-1
+#include <vector>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+int main()
+{
+    std::vector<int> v{1,2,3,4,5};
+    std::deque<int> d;
+    std::copy(v.begin(), v.end(),
+              std::front_insert_iterator<std::deque<int>>(d)); // or std::front_inserter(d)
+    for(int n : d)
+        std::cout << n << ' ';
+    std::cout << '\n';
+}
