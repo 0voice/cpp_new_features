51472ae4fc02489349baeb6ab7e11ccac3ccee4b
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:28:25 +0800

    Update 017_vector_erase_if.cpp

--- cpp_20/017_vector_erase_if.cpp
+++ cpp_20/017_vector_erase_if.cpp
@@ -1,1 +1,27 @@
-1
+#include <map>
+#include <iostream>
+ 
+template<typename Os, typename Container>
+inline Os& operator<<(Os& os, Container const& cont)
+{
+    os << "{";
+    for (const auto& item : cont) {
+        os << "{" << item.first << ", " << item.second << "}";
+    }
+    return os << "}";
+}
+ 
+int main()
+{
+    std::map<int, char> data {{1, 'a'},{2, 'b'},{3, 'c'},{4, 'd'},
+                              {5, 'e'},{4, 'f'},{5, 'g'},{5, 'g'}};
+    std::cout << "Original:\n" << data << '\n';
+ 
+    const auto count = std::erase_if(data, [](const auto& item) {
+        auto const& [key, value] = item;
+        return (key & 1) == 1;
+    });
+ 
+    std::cout << "Erase items with odd keys:\n" << data << '\n'
+              << count << " items removed.\n";
+}
