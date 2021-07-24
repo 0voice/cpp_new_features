64d601a9baecc8a3b546b6554d1a4987f510719a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:42:04 +0800

    Update 021_std_iter_reference_t.cpp

--- cpp_20/021_std_iter_reference_t.cpp
+++ cpp_20/021_std_iter_reference_t.cpp
@@ -1,1 +1,38 @@
-1
+#include <algorithm>
+#include <list>
+#include <vector>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <functional>
+ 
+int main()
+{
+    std::list<int> l(10);
+ 
+    std::iota(l.begin(), l.end(), -4);
+    std::vector<std::reference_wrapper<int>> v(l.begin(), l.end());
+ 
+    // can't use shuffle on a list (requires random access), but can use it on a vector
+    std::shuffle(v.begin(), v.end(), std::mt19937{std::random_device{}()});
+ 
+    std::cout << "Contents of the list: ";
+    for (int n : l){ 
+        std::cout << n << ' ';
+    }
+ 
+    std::cout << "\nContents of the list, as seen through a shuffled vector: ";
+    for (int i : v){
+        std::cout << i << ' ';
+    }
+ 
+    std::cout << "\n\nDoubling the values in the initial list...\n\n";
+    for (int& i : l) {
+        i *= 2;
+    }
+ 
+    std::cout << "Contents of the list, as seen through a shuffled vector: ";
+    for (int i : v){
+       std::cout << i << ' ';
+    }
+}
