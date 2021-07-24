c51b2c117a5c350566d1349473204686cbcc7dfb
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:27:42 +0800

    Update 017_vector_erase.cpp

--- cpp_20/017_vector_erase.cpp
+++ cpp_20/017_vector_erase.cpp
@@ -1,1 +1,25 @@
-1
+#include <iostream> 
+#include <string> 
+using namespace std; 
+  
+// Function to demo erase() 
+void eraseDemo(string str) 
+{ 
+    // Deletes all characters 
+    str.erase(); 
+  
+    cout << "After erase():"; 
+    cout << str; 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    string str("Hello World!"); 
+  
+    cout << "Before erase():"; 
+    cout << str << endl; 
+    eraseDemo(str); 
+  
+    return 0; 
+}
