a9dee18226c4a50b508e407a33524df8593938cf
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:29:59 +0800

    Update 019_unordered_map_erase_if.cpp

--- cpp_20/019_unordered_map_erase_if.cpp
+++ cpp_20/019_unordered_map_erase_if.cpp
@@ -1,1 +1,9 @@
-1
+auto old_size = c.size();
+for (auto i = c.begin(), last = c.end(); i != last; ) {
+  if (pred(*i)) {
+    i = c.erase(i);
+  } else {
+    ++i;
+  }
+}
+return old_size - c.size();
