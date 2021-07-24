218d696f113105061404d509a2f91a7e2348867a
ls-Brynn <87458342+ls-Brynn@users.noreply.github.com>
2021-07-24 21:34:16 +0800

    Update 020_span_end.cpp

--- cpp_20/020_span_end.cpp
+++ cpp_20/020_span_end.cpp
@@ -1,1 +1,17 @@
-1
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+ 
+int main() 
+{
+    std::vector<int> v = { 3, 1, 4 };
+    if (std::find(std::begin(v), std::end(v), 5) != std::end(v)) {
+        std::cout << "found a 5 in vector v!\n";
+    }
+ 
+    int a[] = { 5, 10, 15 };
+    if (std::find(std::begin(a), std::end(a), 5) != std::end(a)) {
+        std::cout << "found a 5 in array a!\n";
+    }
+}
